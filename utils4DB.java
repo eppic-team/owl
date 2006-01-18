e43f3993f3aca4c810482b20d7ed28b3ea0569d8
Ioannis Filippis <filippis@gmail.com>
2006-01-18 19:07:41 +0000

    Adding getDbSizeInfo method into utils4DB program

--- utils4DB.java
+++ utils4DB.java
@@ -3,6 +3,7 @@ package tools.trunk;
 import java.sql.*;
 import java.io.*;
 import java.util.*;
+import java.lang.Math.*;
 
 public class utils4DB {
 
@@ -71,5 +72,48 @@ public class utils4DB {
 	return range;
 
     }
+
+    public static void getDbSizeInfo(String connFile, String DB) {
+
+	Connection myConnection;
+	mySQLConnect SQLC;
+
+	double data = 0, index = 0, table_data = 0, table_index = 0, GB = Math.pow(2, 30);
+	String Query = null, table = null;
+	Statement Stmt = null;
+	ResultSet RS = null;
+
+	SQLC = new mySQLConnect();
+	SQLC.readConnectionFile(connFile);
+	myConnection = SQLC.openConnection();
+
+	try {
+	    Query = "SHOW TABLE STATUS FROM "+DB;
+	    Stmt = myConnection.createStatement();
+	    RS = Stmt.executeQuery(Query);
+	    while (RS.next()) {
+		table = RS.getString("Name");
+		table_data = RS.getDouble("Data_length");
+		table_index = RS.getDouble("Index_length");
+		data += RS.getDouble("Data_length");
+		index += RS.getDouble("Index_length");
+
+		System.out.println("Table "+table+"##data:"+table_data+", index:"+table_index);
+	    }
+	    RS.close();
+	    Stmt.close();
+
+	    System.out.println("Database "+DB+" needs "+((data+index)/GB)+ " GB (data:"+(data/GB)+", index:"+(index/GB)+").");
+	} 
+	catch (SQLException E) {
+	    System.out.println("SQLException: " + E.getMessage());
+	    System.out.println("SQLState:     " + E.getSQLState());
+	    System.out.println("VendorError:  " + E.getErrorCode());
+	} // end try/catch connection
+
+	SQLC.closeConnection(myConnection);
+
+    }
+    
 
 }
