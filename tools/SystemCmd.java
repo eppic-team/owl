491ea988c56cd60b6a6ef34f6cede62c37042ed2
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2006-03-14 09:45:57 +0000

    New exec method taking only a String argument wasn't working. Fixed the bug.

--- tools/SystemCmd.java
+++ tools/SystemCmd.java
@@ -1,37 +1,43 @@
 package tools;
 
 import java.io.*;
- 
+
 public class SystemCmd {
-
-    public static String exec(String[] cmd) {
 	
-	String output = "";
-
-	try {
-
-	    String line = "";
-	    Process p = Runtime.getRuntime().exec(cmd);
-	    BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-	    while ((line = input.readLine()) != null) {
-		output = output + line + "\n";
-	    }
-	    input.close();
-	
-	} catch (Exception err) {
-	    err.printStackTrace();
+	public static String exec(String[] cmd) {		
+		String output = "";
+		try {
+			String line = "";
+			Process p = Runtime.getRuntime().exec(cmd);
+			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			while ((line = input.readLine()) != null) {
+				output = output + line + "\n";
+			}
+			input.close();
+			
+		} catch (Exception err) {
+			err.printStackTrace();
+		}		
+		return output;		
 	}
 	
-	return output;
-
-    }
-    
-    public static String exec(String cmd) {
-    	String[] cmdarr={cmd};
-    	String output=exec(cmdarr);
-    	return output;
-    }
-
-
+	public static String exec(String cmd) {
+		String output = "";
+		try {
+			String line = "";
+			Process p = Runtime.getRuntime().exec(cmd);
+			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			while ((line = input.readLine()) != null) {
+				output = output + line + "\n";
+			}
+			input.close();
+			
+		} catch (Exception err) {
+			err.printStackTrace();
+		}		
+		return output;		
+	}
+	
+	
 }
- 
+
