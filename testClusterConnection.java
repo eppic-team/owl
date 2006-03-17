6fce44e4c137187576866924f5ada29acce48690
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2006-03-17 11:03:15 +0000

    Now using constructor without key and createStatement with key, since createStatement without key is not allowed anymore (is now private)

--- testClusterConnection.java
+++ testClusterConnection.java
@@ -13,14 +13,14 @@ public class testClusterConnection {
 	public static void main(String[] args) {
 		int asuId=30;
 		//ClusterConnection conn=new ClusterConnection();
-		ClusterConnection conn=new ClusterConnection("pdbgraph","asu","duarte","nieve");
+		ClusterConnection conn=new ClusterConnection("pdbgraph","duarte","nieve");
 		System.out.println(conn.db);
 		try {
 			//System.out.println(conn.getHost4Idx(asuId));		
 			Statement S;
 			ResultSet R;
 			//S = conn.createNConStatement("asu",asuId);
-			S=conn.createStatement(asuId);
+			S=conn.createStatement("asu",asuId);
 			System.out.println(conn.host);
 			R=S.executeQuery("SELECT asu_id,accession_code FROM asu_list WHERE asu_id="+asuId+";");
 			System.out.println("asu_id\taccession_code");
