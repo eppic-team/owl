ae69497b8110adf0b293d18296f645050bae49c6
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2006-04-24 12:41:27 +0000

    MAJOR change. Split DataDistribution into 2 classes: DataDistributer and DataDistribution. I haven't actually changed or added functionality DataDistributer deals with the distribution of the data, while DataDistribution deals with things to do when data is already distributed, right now is only a few data checks Note that DataDistributer now has two db fields: srcDb and destDb. This is different to before, when destDb was rather a parameter passed as arguments to the methods Methods in DataDistributer have been tidied up a little (specially load and dump ones)

--- tools/DataDistribution.java
+++ tools/DataDistribution.java
@@ -7,21 +7,18 @@ import java.util.HashMap;
 
 public class DataDistribution {
 
-	final static String GLOBALDIR="/project/snow/global/tmp";
 	final static String ADMINDIR="/project/StruPPi/Cluster/admin";
+	final static String HOSTSFILE=ADMINDIR+"/hosts_ss.txt";
 	public final static String MASTER="white";
-	final static String HFILE=ADMINDIR+"/hosts_ss.txt";
 	final static String KEYMASTERDB="key_master";
 
-	boolean debug=false; // if set to true only mysql commands written, no actual dump or load, also dump directory not removed. Use setDebug method to change it
-	String dumpdir;
-	private String db;
+	public String db;
 	private String user;
 	private String pwd;
+	public boolean isSplit; // for future use 
 	
 
 	public DataDistribution(String db,String user,String pwd) {
-		this.dumpdir=GLOBALDIR+"/dumps_tmp_"+System.currentTimeMillis();
 		this.db=db;
 		this.user=user;
 		this.pwd=pwd;
@@ -42,19 +39,10 @@ public class DataDistribution {
 		return conn;
 	}
 	
-	private MySQLConnection getConnectionToNode(String node, String dbName){
-		MySQLConnection conn=new MySQLConnection(node,user,pwd,dbName);
-		return conn;
-	}
-	
-	public void setDebug(boolean debug) {
-		this.debug=debug;
-	}
-	
 	public static String[] getNodes() {
 		String[] nodes=null;	    
 	    try {
-			File inputFile = new File(HFILE);
+			File inputFile = new File(HOSTSFILE);
 	    	BufferedReader hostsFile = new BufferedReader(new FileReader(inputFile)); // open BufferedReader to the file
 	    	String nodesstr=hostsFile.readLine();
 	    	nodes=nodesstr.split(" ");
@@ -62,121 +50,9 @@ public class DataDistribution {
 	    }
 	    catch (IOException e){
 	    	e.printStackTrace();
-	    	System.err.println("Couldn't read from file "+HFILE);
+	    	System.err.println("Couldn't read from file "+HOSTSFILE);
 	    }
 	    return nodes;
-	}
-
-	public void initializeDirs() {	
-	    if (!((new File(dumpdir)).mkdir())) {
-	    	System.err.println("Couldn't create directory "+dumpdir);
-	    	System.exit(1);
-	    }
-		SystemCmd.exec("chmod go+rw "+dumpdir);
-	}
-	
-	public void finalizeDirs() {
-	    if (debug) {
-	    	System.out.println("Temporary directory "+dumpdir+" was not removed. You must remove it manually");
-	    } else {
-	    	System.out.println("Removing temporary directory "+dumpdir);
-	    	//TODO must capture exit state and print to error if problems deleting dir
-	    	SystemCmd.exec("rm -rf "+dumpdir);
-	    }
-	}
-	
-	public void dumpData(String[] srchosts, String[] tables) {
-		// initialising temporary dump directory
-		initializeDirs();
-		for (String node: srchosts) {
-			dumpData(node,tables);
-		}
-		System.out.println ("Dump finished.");
-	}
-	
-	private void dumpData(String srchost, String[] tables) {
-		//String quickpar="--quick"; // not to buffer to memory, needed for big tables
-		if (!((new File(dumpdir+"/"+srchost+"/"+db)).mkdirs())) {
-			System.err.println("Couldn't create directory "+dumpdir+"/"+srchost+"/"+db);
-			System.exit(1);
-		}
-		SystemCmd.exec("chmod -R go+rw "+dumpdir+"/"+srchost);
-		for ( String tbl: tables) {
-			String outfile=dumpdir+"/"+srchost+"/"+db+"/"+tbl+".txt";
-			String wherestr="";
-			String sqldumpstr="SELECT * FROM `"+tbl+"` "+wherestr+" INTO OUTFILE '"+outfile+"';";
-			//String dumpstr="$MYSQLDIR/bin/mysql $srcconnpar $quickpar -e \"$sqldumpstr\" ";
-			if (debug) {System.out.println ("HOST="+srchost+", sqldumpstr="+sqldumpstr);} 
-			else {
-				try {
-					MySQLConnection conn = this.getConnectionToNode(srchost);
-					Statement S=conn.createStatement();
-					S.executeQuery(sqldumpstr);
-					System.out.println ("Dumped from host="+srchost+", database="+db+", table="+tbl+" to outfile="+outfile);
-					S.close();
-					conn.close();
-				}
-				catch(SQLException e){
-					e.printStackTrace();
-					System.err.println("Couldn't dump from host="+srchost+", database="+db+", table="+tbl+" to outfile="+outfile);
-					System.exit(1);
-				}		
-			} //end else if debug
-		} // end foreach tbl
-	}
-	
-	public void loadData(String[] srchosts, String[] desthosts,String[] tables, String destDb) {
-	    for (String desthost: desthosts) {
-	    	for (String srchost: srchosts) {
-	    		loadData(srchost,desthost,tables,destDb);
-	    	} 
-	    } 
-	    System.out.println ("Load finished.");
-	    finalizeDirs();
-	}
-
-	private void loadData(String srchost, String desthost,String[] tables, String destDb) {
-		for (String tbl:tables) {
-			String dumpfile=dumpdir+"/"+srchost+"/"+db+"/"+tbl+".txt";
-			String sqlloadstr="LOAD DATA INFILE '"+dumpfile+"' INTO TABLE `"+tbl+"`;";
-			if (debug) {System.out.println ("SRCHOST="+srchost+", DESTHOST="+desthost+", sqlloadstr="+sqlloadstr);} 
-			else {
-				try {
-					MySQLConnection conn = this.getConnectionToNode(desthost,destDb);
-					conn.executeSql(sqlloadstr);
-					System.out.println ("HOST: "+desthost+". Loaded from file="+dumpfile+", into database="+destDb+", table="+tbl);
-					conn.close();
-				}
-				catch(SQLException e){
-					e.printStackTrace();
-					System.err.println("Errors occurred while loading data from file="+dumpfile+" into host="+desthost+", database="+destDb+", table="+tbl);
-					System.exit(1);
-				}
-			}	    		
-		} // end foreach tbl
-	}
-	
-	private void loadSplitData(String srchost, String[] desthosts, String tableName, String destDb) {
-		for (String desthost:desthosts) {
-			String dumpfile=dumpdir+"/"+srchost+"/"+db+"/"+tableName+"_split_"+desthost+".txt";
-			String sqlloadstr="LOAD DATA INFILE '"+dumpfile+"' INTO TABLE `"+tableName+"`;";
-			if (debug) {System.out.println ("SRCHOST="+srchost+", DESTHOST="+desthost+", sqlloadstr="+sqlloadstr);} 
-			else {
-				try {
-					MySQLConnection conn = this.getConnectionToNode(desthost,destDb);
-					conn.executeSql(sqlloadstr);
-					System.out.println ("HOST: "+desthost+". Loaded from file="+dumpfile+", database="+db+", table="+tableName);
-					conn.close();
-				}
-				catch(SQLException e){
-					e.printStackTrace();
-					System.err.println("Errors occurred while loading data from file="+dumpfile+" into host="+desthost+", database="+destDb+", table="+tableName);
-					System.exit(1);
-				}
-			}	    		
-		} // end foreach desthosts
-	    System.out.println ("Load finished.");
-	    finalizeDirs();
 	}
 	
 	public boolean checkCountsAllTables (){
@@ -396,50 +272,8 @@ public class DataDistribution {
 	}
 	
 	/**
-	 * Method used by splitIdsIntoSet and getIdSetsFromNodes. 
-	 * To get all ordered ids from a certain key and table from this db in a certain host
-	 * @param key the key name
-	 * @param table the table name
-	 * @param host the host name
-	 * @return int array containing all ids 
-	 */
-	public int[] getAllIds4KeyAndTable(String key, String table, String host){
-		int[] allIds=null;		
-		try {
-			MySQLConnection conn=this.getConnectionToNode(host);
-			Statement S=conn.createStatement();
-			String query="SELECT DISTINCT "+key+" FROM "+table+" ORDER BY "+key+";";
-			ResultSet R=S.executeQuery(query);
-			ArrayList<Integer> idsAL=new ArrayList<Integer>();
-			while (R.next()){
-				idsAL.add(R.getInt(1));
-			}
-			allIds=new int[idsAL.size()];
-			for (int i=0;i<idsAL.size();i++) {
-				allIds[i]=idsAL.get(i);
-			}
-			R.close();
-			S.close();
-			conn.close();
-		}
-		catch (SQLException e){
-			e.printStackTrace();
-		}
-		return allIds;
-	}
-	
-	/**
-	 * The two following methods getIdSetsFromNodes and splitIdsIntoSets are very related.
-	 * Both return the same thing: a "data distribution", which is defined as a HashMap of int[], 
-	 * being the keys the name of the nodes and the int[] all ids that the node contains.
-	 * Maybe should create a class that encapsulates the "data distribution" HashMap
-	 */
-	
-	/**
 	 * For a certain key and table finds out how the table is splitted in nodes and returns the "data distribution"
-	 * To be used when data for a table is already splitted (splitted doesn't need to mean that there is different chunks of it
-	 * in different nodes, but rather that the same table is somehow in all nodes (either in chunks or the whole thing)
-	 * Take care when using this method as an argument of insertIdsToKeyMaster: if table is not in chunks (i.e. all data in all)
+	 * Take care when using this method as an argument of insertIdsToKeyMaster: if table is not in chunks (but rather all data in all)
 	 * then ids can't be inserted in key_master as there will be duplication, i.e. for key_id=1 as data is in all nodes there 
 	 * will be 40 copies of it and thus 40 equal ids will try to be inserted in key_master, which a) makes no sense and 
 	 * b) mysql won't do it anyway  
@@ -451,281 +285,11 @@ public class DataDistribution {
 		HashMap<String,int[]> idSets =new HashMap<String,int[]>();
 		String[] nodes = getNodes();
 		for (String node:nodes){
-			idSets.put(node,getAllIds4KeyAndTable(key,table,node));
+			MySQLConnection conn = this.getConnectionToNode(node);
+			idSets.put(node,conn.getAllIds4KeyAndTable(key,table));
+			conn.close();		
 		}
 		return idSets;
 	}
 	
-	/**
-	 * For a certain key and table returns a certain "data distribution" (kind of evenly distributed) of the data to the nodes
-	 * To be used when we have a table that we are going to split to the nodes
-	 * TODO eventually the code could be cleverer so that the data is actually evenly distributed, right now is only evenly distributed on key ids
-	 * @param key
-	 * @param table
-	 * @return idSets HashMap, keys are node names, values: int array with the ids for each node
-	 */
-	public HashMap<String,int[]> splitIdsIntoSets(String key, String table){
-		HashMap<String,int[]> idSets =new HashMap<String,int[]>();
-		String[] nodes=DataDistribution.getNodes();
-		int numNodes=nodes.length;
-		int[] allIds=this.getAllIds4KeyAndTable(key,table,MASTER);
-		int numIds=allIds.length;
-		int setSize=numIds/numNodes;
-		int remainder=numIds%numNodes;
-		for (int i=0;i<numNodes;i++){
-			if (i<remainder){ // for the first "remainder" number of nodes we put setSize+1 ids in the node
-				int[] thisnodeidset=new int[setSize+1];
-				for (int j=0;j<thisnodeidset.length;j++){
-					thisnodeidset[j]=allIds[j+i*(setSize+1)];
-				}
-				idSets.put(nodes[i],thisnodeidset);
-			} else {         // for the rest we put only setSize ids
-				int[] thisnodeidset=new int[setSize]; 
-				for (int j=0;j<thisnodeidset.length;j++){
-					thisnodeidset[j]=allIds[j+remainder*(setSize+1)+(i-remainder)*setSize];
-				}
-				idSets.put(nodes[i],thisnodeidset); 
-			}
-		}		
-		return idSets;
-	}
-
-	/**
-	 * To split a given table in chunks based on a key, split tables remain in same database and server
-	 * @param key
-	 * @param table 
-	 */
-	public void splitTable (String key,String table){
-		String query;
-		HashMap<String,int[]> idSets = this.splitIdsIntoSets(key,table);
-		String[] splitTables=new String[idSets.size()];
-		try {
-			MySQLConnection conn=this.getConnectionToMaster();
-			int i=0;
-			for (String node:idSets.keySet()) {
-				String splitTbl=table+"_split_"+node;
-				splitTables[i]=splitTbl;
-				i++;
-				// we create permanent tables
-				query="CREATE TABLE "+splitTbl+" LIKE "+table+";";
-				conn.executeSql(query);
-				// drop the indexes if there was any, indexes will slow down the creation of split tables
-				String[] indexes=conn.getAllIndexes4Table(table);
-				for (String index:indexes) { 
-					conn.executeSql("DROP INDEX "+index+" ON "+splitTbl+";");
-				}
-				int idmin=idSets.get(node)[0];
-				int idmax=idSets.get(node)[idSets.get(node).length-1];
-				query="INSERT INTO "+splitTbl+" SELECT * FROM "+table+" WHERE "+key+">="+idmin+" AND "+key+"<="+idmax+";";				
-				conn.executeSql(query);
-				//TODO recreate indexes, use method getCreateIndex4Table from MySQLConnection
-			}
-			conn.close();
-		}
-		catch (SQLException e){
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * To split a given table in chunks based on a key, split tables go to different nodes of cluster 
-	 * @param key
-	 * @param table
-	 * @param destDb name of destination db 
-	 */
-	public void splitTableToCluster (String key,String table, String destDb){
-		String query;
-		HashMap<String,int[]> idSets = this.splitIdsIntoSets(key,table);
-		String[] splitTables=new String[idSets.size()];
-		try {
-			MySQLConnection conn=this.getConnectionToMaster();
-			int i=0;
-			for (String node:idSets.keySet()) {
-				String splitTbl=table+"_split_"+node;
-				splitTables[i]=splitTbl;
-				i++;
-				// we create permanent tables, later we drop them. Can't be temporary as we use another connection for dumpData
-				query="CREATE TABLE "+splitTbl+" LIKE "+table+";";
-				conn.executeSql(query);
-				// drop the indexes if there was any, indexes will slow down the creation of split tables
-				String[] indexes=conn.getAllIndexes4Table(table);
-				for (String index:indexes) { 
-					conn.executeSql("DROP INDEX "+index+" ON "+splitTbl+";");
-				}
-				// make the table a memory table (won't be feasible in general case where tables can be VERY big, even white won't cope) 
-				//query="ALTER TABLE "+splitTbl+" TYPE=MEMORY;";
-				//conn.executeSql(query);
-				int idmin=idSets.get(node)[0];
-				int idmax=idSets.get(node)[idSets.get(node).length-1];
-				query="INSERT INTO "+splitTbl+" SELECT * FROM "+table+" WHERE "+key+">="+idmin+" AND "+key+"<="+idmax+";";				
-				conn.executeSql(query);
-			}
-			// transfering data across
-			String[] srchosts={MASTER};
-			String[] desthosts=getNodes();
-			dumpData(srchosts,splitTables);
-			// using here loadSplitData rather than loadData because table names are not the same on source and destination, i.e. source: table_split_tla01, dest: table
-			loadSplitData(MASTER,desthosts,table,destDb);
-			// droping table, we don't want them anymore after loading data to nodes
-			for (String tbl:splitTables){					
-				query="DROP TABLE "+tbl+";";
-				conn.executeSql(query);
-			}
-			conn.close();
-		}
-		catch (SQLException e){
-			e.printStackTrace();
-		}
-		// putting the ids in the key_master database so we keep track of where everything is
-		insertIdsToKeyMaster(key,table,destDb,idSets);
-	}
-	
-	/**
-	 * Insert all ids to the key_master database creating a new table for this table/destDb combination if not exists
-	 * @param key name of key on which distribution of table is based
-	 * @param table name of table that we are distributing
-	 * @param destDb name of database in nodes where data is distributed
-	 * @param idSets as returned from splitIdsIntoSets or getIdSetsFromNodes
-	 */
-	public void insertIdsToKeyMaster(String key,String table,String destDb,HashMap<String,int[]> idSets) {
-		MySQLConnection conn = this.getConnectionToMasterKeyDb();		
-		String keyMasterTbl = createNewKeyMasterTbl(key,table,destDb);
-		removePK(keyMasterTbl,key); // attention removing primary keys, duplicates won't be checked!!!
-		for (String node:idSets.keySet()){
-			int[] thisNodeIds=idSets.get(node);
-			for (int id:thisNodeIds){
-				String query="INSERT INTO "+keyMasterTbl+" ("+key+",client_id) " +
-							"SELECT "+id+",c.client_id FROM clients_names AS c WHERE client_name='"+node+"';";
-				try {
-					conn.executeSql(query);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		removeZeros(keyMasterTbl,key); // we only have inserted 0s for records that we didn't want, it's safe now to get rid of them
-		addPK(keyMasterTbl,key); // if there were duplicates, this should barf
-	}
-
-	/**
-	 * To create a new key master table in the key_master database given a key, table and db. Used by insertIdsToKeyMaster
-	 * Eventually this method on other key_master related should go into their own class, shouldn't they?
-	 * @param key
-	 * @param table
-	 * @param destDb the db
-	 * @return the name of the key master table created
-	 */
-	public String createNewKeyMasterTbl(String key,String table,String destDb) {
-		String keyMasterTbl=destDb+"__"+table;
-		MySQLConnection conn=this.getConnectionToMasterKeyDb();
-		try {
-			String query="CREATE TABLE IF NOT EXISTS "+keyMasterTbl+" ("+
-						key+" int(11) NOT NULL auto_increment, " +
-						"client_id smallint(6) NOT NULL default '0', " +
-						"PRIMARY KEY (`"+key+"`) " +
-						") ENGINE=MyISAM DEFAULT CHARSET=ascii COLLATE=ascii_bin;";
-			Statement S=conn.createStatement();
-			S.executeUpdate(query);
-			S.close();
-		} catch (SQLException e) {
-			System.err.println("Couldn't create table "+keyMasterTbl);
-			e.printStackTrace();
-		}
-		try {
-			Statement S=conn.createStatement();
-			String query="INSERT INTO dbs_keys (key_name,db,key_master_table) VALUES (\'"+key+"\',\'"+db+"\',\'"+keyMasterTbl+"\');";
-			S.executeUpdate(query);
-			S.close();
-		} catch (SQLException e) {
-			System.err.println("Didn't insert new record into table dbs_keys of database: "+KEYMASTERDB+". The record for key: "+key+", table: "+table+" existed already. This is usually a harmless error!");
-			System.err.println("SQLException: " + e.getMessage());
-		}
-		conn.close();
-		return keyMasterTbl;
-	}
-	
-	public void removePK (String keyMasterTbl,String key){
-		MySQLConnection conn=this.getConnectionToMasterKeyDb();		
-		try {
-			String query="ALTER TABLE "+keyMasterTbl+" MODIFY "+key+" int(11) NOT NULL default '0';";
-			conn.executeSql(query);
-			query="ALTER TABLE "+keyMasterTbl+" DROP PRIMARY KEY;";
-			conn.executeSql(query);			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		conn.close();
-	}
-	
-	public void addPK (String keyMasterTbl, String key){
-		MySQLConnection conn=this.getConnectionToMasterKeyDb();		
-		try {
-			String query="ALTER TABLE "+keyMasterTbl+" ADD PRIMARY KEY("+key+");";				
-			conn.executeSql(query);
-			query="ALTER TABLE "+keyMasterTbl+" MODIFY "+key+" int(11) NOT NULL auto_increment;";
-			conn.executeSql(query);			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		conn.close();		
-	}
-	
-	public void removeZeros (String keyMasterTbl, String key){
-		MySQLConnection conn=this.getConnectionToMasterKeyDb();		
-		try {
-			String query="DELETE FROM "+keyMasterTbl+" WHERE "+key+"=0;";				
-			conn.executeSql(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		conn.close();				
-	}
-
-	/**
-	 * Executes a query in all nodes in cluster.
-	 * TODO Right now it is serial, must parallelize this with threads
-	 * @param query
-	 */
-	public void clusterExecuteQuery(String query){
-		String[] nodes = getNodes();
-		for (String node: nodes){
-			try {
-				MySQLConnection conn = this.getConnectionToNode(node);
-				conn.executeSql(query);
-				conn.close();
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-				System.err.println("Couldn't execute query="+query+", in node="+node);
-				System.exit(1);
-			}
-		}
-
-	}
-	
-	/**
-	 * Executes a query in all nodes in cluster given a HashMap containing a set of queries (one per node)
-	 * TODO Right now it is serial, must parallelize this with threads
-	 * TODO This can be used in the load/dump methods in this class where queries are different for each node
-	 * @param queries a HashMap containing a query per node
-	 */
-	public void clusterExecuteQuery(HashMap<String,String> queries){
-		String[] nodes = getNodes();
-		for (String node: nodes){
-			String query="";
-			try {
-				query=queries.get(node);
-				MySQLConnection conn = this.getConnectionToNode(node);
-				conn.executeSql(query);
-				conn.close();
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-				System.err.println("Couldn't execute query="+query+", in node="+node);
-				System.exit(1);
-			}
-		}
-
-	}
-
 }
