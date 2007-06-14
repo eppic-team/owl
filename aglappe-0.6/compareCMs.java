667693ec394fe4813c394f2687d840a49275028e
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2007-06-14 14:44:44 +0000

    Tagging version 0.6 for release of cmview 0.6

---
+++ aglappe-0.6/compareCMs.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+
+import proteinstructure.Graph;
+import proteinstructure.MsdsdAcCodeNotFoundError;
+import proteinstructure.MsdsdInconsistentResidueNumbersError;
+import proteinstructure.Pdb;
+import proteinstructure.PdbaseAcCodeNotFoundError;
+import proteinstructure.PdbaseInconsistencyError;
+import java.util.HashMap;
+
+
+public class compareCMs {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception, IOException, PdbaseInconsistencyError, PdbaseAcCodeNotFoundError, MsdsdAcCodeNotFoundError, MsdsdInconsistentResidueNumbersError{
+		
+		String onlyIn1File="onlyin1.graph";
+		String onlyIn2File="onlyin2.graph";
+		String commonFile="common.graph";
+		
+		
+		String pdbcode1="1ses";
+		String chaincode1="A";
+		String pdbcode2="1set";
+		String chaincode2="A";
+
+		
+		System.out.println("loading structures from pdbase");
+		Pdb pdb1 = new Pdb(pdbcode1,chaincode1); // pdbase is default source for constructor, a name for a pdbase database can also be passed
+		Pdb pdb2 = new Pdb(pdbcode2,chaincode2);
+
+		System.out.println("getting graphs");
+		Graph graph1 = pdb1.get_graph("ALL", 4.2);
+		Graph graph2 = pdb2.get_graph("ALL", 4.2);
+
+		HashMap<String,Graph> comparison = graph1.compare(graph2);
+		
+		Graph onlyIn1 = comparison.get("onlythis");
+		Graph onlyIn2 = comparison.get("onlyother");
+		Graph common = comparison.get("common");
+		
+		System.out.println("writing output files "+onlyIn1File+", "+onlyIn2File+", "+commonFile);
+		onlyIn1.write_graph_to_file(onlyIn1File);
+		onlyIn2.write_graph_to_file(onlyIn2File);
+		common.write_graph_to_file(commonFile);
+		
+		
+
+	}
+
+}
---
+++ aglappe-0.6/exchangeOne.java
@@ -0,0 +1,150 @@
+import tools.MySQLConnection;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class exchangeOne {
+
+	/**
+	 * info loss / entropy calculations given a nbhoodstring 
+	 * exchange of one neighbor at a time by a common neighbor 
+	 * @author lappe
+	 */
+	
+	static String user = "lappe"	; // change user name!!
+	static MySQLConnection conn;
+	static double lastFreq, lastAUC; 
+	static int lastRank, lastTotal; 
+	
+	public static void main(String[] args) {
+		double entropy = 0.0, newentropy=0.0, addentropy=0.0, gain; 
+		double delFreq, orgFreq, delAUC, orgAUC; 
+		int delRank, orgRank, delTotal, orgTotal;
+		String nbhood = "", front, middle, tail, newhood="", addhood="", central ="I";
+		if (args.length<2){
+			System.err.println("The starting neighborhood-string and central residue type needs to be given .... i.e. %K%D%L%I%x%D%C% I");
+			System.exit(1);
+		}		
+		nbhood = args[0];
+		central = args[1]; 
+		
+		conn = new MySQLConnection("white",user,"nieve","pdb_reps_graph_4_2_a"); 
+		int l = (int)((nbhood.length()-1)/2); 
+		
+			System.out.println("XChange1 ");
+			System.out.print("0 - ("+nbhood+")("+l+") "); 
+			entropy = getEntropy( nbhood, central); 
+			
+			// System.out.println("Symbols in nbhood :"+l); 
+			orgFreq = lastFreq;
+			orgAUC = lastAUC; 
+			orgRank = lastRank; 
+			orgTotal = lastTotal; 
+			System.out.println("central "+central+". \tt="+orgTotal+" \tentropy = "+String.format("%.5f", entropy)+" \trank#"+String.format("%2d",lastRank)+" \tp("+central+") = "+String.format("%.5f",lastFreq)+" \t\tAUC = "+String.format("%.5f",lastAUC));
+//			 System.out.println("Symbols in nbhood :"+l);
+			
+			for (int i = 0; i<l; i++) {
+				front = nbhood.substring(0,i*2); 
+				middle = nbhood.substring(i*2, i*2+2); 
+				tail = nbhood.substring(i*2+2);
+				System.out.print((i+1)+" - "+front+"("+middle+")"+tail);
+				
+				newhood = front+tail; 
+				
+				System.out.print( " -> " + newhood); 
+				newentropy = getEntropy( newhood, central); 
+				gain = newentropy-entropy; 
+				System.out.print( "\t"+lastTotal+"("+(lastTotal-orgTotal)+")"); 
+				System.out.print( "\t"+String.format("%.5f", newentropy) +" ("+String.format("%.5f", gain)+")");
+				System.out.print( "\t#"+String.format("%2d",lastRank) +" ("+String.format("%2d",(lastRank-orgRank))+")");
+				System.out.print( "\t"+String.format("%.5f", lastFreq) +" ("+String.format("%.5f", (lastFreq-orgFreq))+")");
+				System.out.print( "\t"+String.format("%.5f", lastAUC) +" ("+String.format("%.5f", (lastAUC-orgAUC))+")");
+				System.out.println( " :");
+				// save current values after deleteiong a neighbour to del* 
+				delFreq = lastFreq;
+				delAUC = lastAUC; 
+				delRank = lastRank; 
+				delTotal = lastTotal; 
+				
+				// now add each cn, one at a time  
+				// get all the indirect nbs
+				System.out.print("\tadding d"); 
+				// insert them into the newhood -> addhood
+				addhood = "%d"+newhood; 
+				System.out.print(" +> "+addhood);
+				// claculate entropy 
+				addentropy = getEntropy( addhood, central);  
+				System.out.print( "\t"+lastTotal+"("+(lastTotal-delTotal)+")"); 
+				System.out.print( "\t"+String.format("%.5f", addentropy) +" ("+String.format("%.5f", (addentropy-newentropy))+")");
+				System.out.print( "\t#"+String.format("%2d",lastRank)+" ("+String.format("%2d",(lastRank-delRank))+")");
+				System.out.print( "\t"+String.format("%.5f", lastFreq) +" ("+String.format("%.5f", (lastFreq-delFreq))+")");
+				System.out.print( "\t"+String.format("%.5f", lastAUC) +" ("+String.format("%.5f", (lastAUC-delAUC))+")");
+
+				System.out.println( ".");  
+			} // next symbol in nbhood;  
+		
+		System.out.println("fin."); 
+	}	
+
+		
+	
+	public static double getEntropy( String nbs, String centRes) {
+		String sql, res; 
+		Statement stmt;  
+		ResultSet rsst;
+		double p, psum=0.0, logp, plogp, plogpsum=0.0; 
+		int num; 
+		try {
+			sql = "select count(*) from single_model_node where n like '"+nbs+"';";
+			// System.out.println( sql); 
+			stmt = conn.createStatement();
+			rsst = stmt.executeQuery(sql);
+			if (rsst.next()) lastTotal = rsst.getInt( 1); 
+			rsst.close(); 
+			stmt.close(); 
+			
+			sql = "select res, count(*) as t, count(*)/"+lastTotal+" as p from single_model_node where n like '"+nbs+"' group by res order by p DESC;";
+			stmt = conn.createStatement();
+			rsst = stmt.executeQuery(sql);
+			// System.out.println("rank : res : total t : fraction p : log2(p) : -p*log2(p)");
+			int rank = 0; 
+			boolean seenCentRes = false;  
+			lastAUC = 0.0; 
+			lastRank = 0; 
+			lastFreq = 0.0; 
+			while (rsst.next()) {	
+				rank ++;
+				res = rsst.getString(1); // 1st column -- res
+				num = rsst.getInt(2); // 2nd column -- num
+				p = rsst.getDouble(3); // 3rd: fraction p 
+				// System.out.print(rank+ " : " + res+"   : "+num+ " : " + p);
+				logp = Math.log(p)/Math.log(2.0); // to basis 2 for info in bits 
+				// System.out.print(" : " + logp); 
+				plogp = -1.0 * p * logp;  
+				// System.out.print(" : " + plogp);
+				plogpsum += plogp;  
+				psum += p; 
+				
+				if (res.equals(centRes)) { 
+					// System.out.print(" <==" + centRes);
+					seenCentRes = true;
+					lastFreq = p;
+					lastRank = rank; 
+				}
+				if (seenCentRes) lastAUC += p; 
+				// System.out.println("");
+			}
+			// System.out.println("Sum :"+lastTotal+"       : "+psum+"       : "+plogpsum);
+			rsst.close(); 
+			stmt.close(); 
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("SQLException: " + e.getMessage());
+			System.err.println("SQLState:     " + e.getSQLState());
+		}
+		return plogpsum; 
+	} // end of getEntropy 
+
+}
