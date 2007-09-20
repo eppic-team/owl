e1e7538fcfbf089fbb7fa641fa68942d298017c0
Henning Stehr <h.stehr@gmail.com>
2007-09-20 10:48:55 +0000

    added parsing of residue selection strings

--- proteinstructure/NodeSet.java
+++ proteinstructure/NodeSet.java
@@ -2,11 +2,13 @@ package proteinstructure;
 
 import java.util.TreeSet;
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NodeSet extends TreeSet<Integer> {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	/**
 	 * Returns the set as a vector of intervals of consecutive elements.
 	 * @return
@@ -34,6 +36,42 @@ public class NodeSet extends TreeSet<Integer> {
 		// output last interval
 		intervals.add(new Interval(start, last));
 		return intervals;
+	}
+	
+	/**
+	 * Returns true if selStr is a valid selection string in 'comma-hyphen' syntax, e.g. 1-3,5,7-8.
+	 * @return true if selStr is a syntactically correct selection string, false otherwise
+	 */
+	public static boolean isValidSelectionString(String selStr) {
+		Pattern p = Pattern.compile("\\d+(-\\d+)?(,\\d+(-\\d+)?)*");
+		Matcher m = p.matcher(selStr);
+		return m.matches();
+	}
+	
+	/**
+	 * Create a new NodeSet from a selection string in 'comma-hyphen' nomenclature, e.g. 1-3,5,7-8.
+	 * The validity of a selection string can be checked by isValidSelectionString().
+	 * @return A node set corresponding to the given selection string or null of string is invalid.
+	 */
+	public static NodeSet parseSelectionString(String selStr) {
+		if(!isValidSelectionString(selStr)) return null;
+		NodeSet newSet = new NodeSet();
+		String[] tokens = selStr.split(",");
+		for(String t:tokens) {
+			if(t.contains("-")) {
+				String[] range = t.split("-");
+				int from = Integer.parseInt(range[0]);
+				int to = Integer.parseInt(range[1]);
+				for(int i=from; i <= to; i++) {
+					newSet.add(i);
+				}
+				
+			} else {
+				int num = Integer.parseInt(t);
+				newSet.add(num);
+			}
+		}
+		return newSet;
 	}
 	
 }
