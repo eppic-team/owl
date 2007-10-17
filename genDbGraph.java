4415ff9afa2d0b91aeae43dd8a84370590e297ce
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2007-10-17 15:45:30 +0000

    Now can do multiple contact types/cutoffs per pdb structure

--- genDbGraph.java
+++ genDbGraph.java
@@ -50,7 +50,9 @@ public class genDbGraph {
 				"1)  genDbGraph -i <listfile> -d <distance_cutoff> -t <contact_type> -o <output_db> [-D <pdbase_db>] \n" +
 				"2)  genDbGraph -p <pdb_code> -c <chain_pdb_code> -d <distance_cutoff> -t <contact_type> -o <output_db> [-D <pdbase_db>] \n" +
 				"3)  genDbGraph -f <pdbfile> -c <chain_pdb_code> -d <distance_cutoff> -t <contact_type> -o <output_db> \n" +
-				"In case 2) also a list of comma separated pdb codes and chain codes can be specified, e.g. -p 1bxy,1jos -c A,A\n" +
+				"\nA comma separated list of contact types and distance cutoffs can be given instead of just 1, e.g. -d 8.0,8.5 -t Ca,Cb will generate the graphs for Ca at 8.0 and for Cb at 8.5\n" +
+				"If only 1 contact type given and multiple cutoffs, graphs will be generated at all the cutoffs for the one contact type\n"+
+				"\nIn case 2) also a list of comma separated pdb codes and chain codes can be specified, e.g. -p 1bxy,1jos -c A,A\n" +
 				"\nIf pdbase_db not specified, the default pdbase will be used\n" +
 				"\nSecondary structure will be taken from pdbase database. If reading from pdb file and the pdb file is missing the secondary structure, then it will be assigned using dssp\n"; 
 
@@ -59,8 +61,8 @@ public class genDbGraph {
 		String[] pdbChainCodes = null;
 		String pdbfile = "";
 		String pdbaseDb = PDB_DB;
-		String edgeType = "";
-		double cutoff = 0.0;
+		String[] edgeTypes = null;
+		double[] cutoffs = null;
 		String outputDb = "";
 		
 		Getopt g = new Getopt("genDbGraph", args, "i:p:c:f:d:t:o:D:h?");
@@ -80,10 +82,14 @@ public class genDbGraph {
 				pdbfile = g.getOptarg();
 				break;
 			case 'd':
-				cutoff = Double.valueOf(g.getOptarg());
+				String[] cutoffsStr = g.getOptarg().split(",");
+				cutoffs = new double[cutoffsStr.length];
+				for (int i =0;i<cutoffs.length;i++) {
+					cutoffs[i] = Double.valueOf(cutoffsStr[i]);
+				}
 				break;
 			case 't':
-				edgeType = g.getOptarg();
+				edgeTypes = g.getOptarg().split(",");
 				break;
 			case 'o':
 				outputDb = g.getOptarg();
@@ -99,20 +105,34 @@ public class genDbGraph {
 			}
 		}
 
-		if (outputDb.equals("") || edgeType.equals("") || cutoff==0.0) {
-			System.err.println("Some missing option");
+		if (outputDb.equals("") || edgeTypes==null || cutoffs==null) {
+			System.err.println("Some missing option\n");
+			System.err.println(help);
+			System.exit(1);
+		}
+		if (edgeTypes.length!=cutoffs.length && edgeTypes.length!=1) {
+			System.err.println("Not same number of contact types as cutoffs given\n");
 			System.err.println(help);
 			System.exit(1);
 		}
 		if (listfile.equals("") && pdbCodes==null && pdbfile.equals("")){
-			System.err.println("Either a listfile, some pdb codes/chain codes or a pdbfile must be given");
+			System.err.println("Either a listfile, some pdb codes/chain codes or a pdbfile must be given\n");
 			System.err.println(help);
 			System.exit(1);
 		}
 		if ((!listfile.equals("") && pdbCodes!=null) || (!listfile.equals("") && !pdbfile.equals("")) || (pdbCodes!=null && !pdbfile.equals(""))) {
-			System.err.println("Options -p/-c, -i and -f/-c are exclusive. Use only one of them");
+			System.err.println("Options -p/-c, -i and -f/-c are exclusive. Use only one of them\n");
 			System.err.println(help);
 			System.exit(1);			
+		}
+		
+		// setting edgeTypes in case only 1 was given with multiple cutoffs
+		if (edgeTypes.length==1 && cutoffs.length>1) {
+			String edgeType = edgeTypes[0];
+			edgeTypes = new String[cutoffs.length];
+			for (int i=0;i<cutoffs.length;i++){
+				edgeTypes[i] = edgeType;
+			}
 		}
 
 		
@@ -155,21 +175,22 @@ public class genDbGraph {
 				String pdbChainCode = pdbChainCodes[i];
 
 				try {
-					
-					long start = System.currentTimeMillis();
+
+					System.out.println("Getting pdb data for "+pdbCode+"_"+pdbChainCode);
 					
 					Pdb pdb = new PdbasePdb(pdbCode, pdbChainCode, pdbaseDb, conn);
 
-					// get graph
-					Graph graph = pdb.get_graph(edgeType, cutoff);
+					// get graphs
+					for (int j = 0; j<edgeTypes.length; j++) {
+						System.out.println("--> graph "+edgeTypes[j]+" for cutoff "+cutoffs[j]);
+						
+						Graph graph = pdb.get_graph(edgeTypes[j], cutoffs[j]);
 
-					graph.write_graph_to_db(conn,outputDb);
-
-					long end = System.currentTimeMillis();
-					double time = (double) (end -start)/1000;
+						graph.write_graph_to_db(conn,outputDb);
+						
+						numPdbs++;
+					}
 					
-					System.out.printf(pdbCode+"_"+pdbChainCode+" - %5.3f s\n",time);
-					numPdbs++;
 					
 				} catch (PdbaseInconsistencyError e) {
 					System.err.println("Inconsistency in " + pdbCode + pdbChainCode);
@@ -184,24 +205,32 @@ public class genDbGraph {
 			}
 
 			// output results
-			System.out.println("Number of structures loaded successfully: " + numPdbs);
+			System.out.println("Number of graphs loaded successfully: " + numPdbs);
 
 
 		} else {
 			String pdbChainCode = pdbChainCodes[0];
 			try {
+				
+				System.out.println("Getting chain "+pdbChainCode+" from pdb file "+pdbfile);
+				
 				Pdb pdb = new PdbfilePdb(pdbfile,pdbChainCode);
 				if (!pdb.hasSecondaryStructure()) {
 					pdb.runDssp(DSSP_EXE, DSSP_PARAMS);
 				}
-				Graph graph = pdb.get_graph(edgeType, cutoff);
-				try {
-					graph.write_graph_to_db(conn, outputDb);
-					System.out.println("Loaded to database graph for file "+pdbfile);
-				} catch (SQLException e) {
-					e.printStackTrace();
+				
+				// get graphs
+				for (int j = 0; j<edgeTypes.length; j++) {
+					System.out.println("--> graph "+edgeTypes[j]+" for cutoff "+cutoffs[j]);
+					
+					Graph graph = pdb.get_graph(edgeTypes[j], cutoffs[j]);
+
+					graph.write_graph_to_db(conn,outputDb);
 				}
 				
+				
+			} catch (SQLException e) {
+				System.err.println("Couldn't write graph to db, error: "+e.getMessage());
 			} catch (PdbfileFormatError e) {
 				System.err.println("pdb file "+pdbfile+" doesn't have right format");
 			} catch (PdbChainCodeNotFoundError e) {
