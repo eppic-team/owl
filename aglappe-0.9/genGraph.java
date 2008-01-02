43143687de154db6cdc756c8f201a9d2a85c831b
Henning Stehr <h.stehr@gmail.com>
2008-01-02 14:02:55 +0000

    Tagging version 0.9 to use with CMView 0.9 (same as aglappe-jung 0.1.2 but moved to trunk)

---
+++ aglappe-0.9/genGraph.java
@@ -0,0 +1,214 @@
+import gnu.getopt.Getopt;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import proteinstructure.Pdb;
+import proteinstructure.PdbCodeNotFoundError;
+import proteinstructure.PdbLoadError;
+import proteinstructure.PdbasePdb;
+import proteinstructure.PdbfilePdb;
+import proteinstructure.RIGraph;
+import tools.MySQLConnection;
+
+
+public class genGraph {
+	/*------------------------------ constants ------------------------------*/
+	
+	public static final String			PDB_DB = "pdbase";
+	public static final String			DB_HOST = "white";								
+	public static final String			DB_USER = getUserName();
+	public static final String			DB_PWD = "nieve";
+	public static final String			DSSP_EXE = "/project/StruPPi/bin/dssp";
+	public static final String			DSSP_PARAMS = "--";
+	public static final String			NACCESS_EXE = "/project/StruPPi/bin/naccess";
+	public static final String			NACCESS_PARAMS = "";
+
+	//public static double			cutoff = 4.2;
+	//public static String			edgeType = "ALL";
+	
+	/*---------------------------- private methods --------------------------*/
+	/** 
+	 * Get user name from operating system (for use as database username). 
+	 * */
+	private static String getUserName() {
+		String user = null;
+		user = System.getProperty("user.name");
+		if(user == null) {
+			System.err.println("Could not get user name from operating system.");
+		}
+		return user;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		String help = "Usage, 3 options:\n" +
+				"1)  genGraph -i <listfile> -d <distance_cutoff> -t <contact_type> -o <output_dir> [-D <pdbase_db>] \n" +
+				"2)  genGraph -p <pdb_code> -c <chain_pdb_code> -d <distance_cutoff> -t <contact_type> -o <output_dir> [-D <pdbase_db>] \n" +
+				"3)  genGraph -f <pdbfile> -c <chain_pdb_code> -d <distance_cutoff> -t <contact_type> -o <output_dir> \n" +
+				"In case 2) also a list of comma separated pdb codes and chain codes can be specified, e.g. -p 1bxy,1jos -c A,A\n" +
+				"If pdbase_db not specified, the default pdbase will be used\n"; 
+
+		String listfile = "";
+		String[] pdbCodes = null;
+		String[] pdbChainCodes = null;
+		String pdbfile = "";
+		String pdbaseDb = PDB_DB;
+		String edgeType = "";
+		double cutoff = 0.0;
+		String outputDir = "";
+		
+		Getopt g = new Getopt("genGraph", args, "i:p:c:f:d:t:o:D:h?");
+		int c;
+		while ((c = g.getopt()) != -1) {
+			switch(c){
+			case 'i':
+				listfile = g.getOptarg();
+				break;
+			case 'p':
+				pdbCodes = g.getOptarg().split(",");
+				break;
+			case 'c':
+				pdbChainCodes = g.getOptarg().split(",");
+				break;
+			case 'f':
+				pdbfile = g.getOptarg();
+				break;
+			case 'd':
+				cutoff = Double.valueOf(g.getOptarg());
+				break;
+			case 't':
+				edgeType = g.getOptarg();
+				break;
+			case 'o':
+				outputDir = g.getOptarg();
+				break;
+			case 'D':
+				pdbaseDb = g.getOptarg();
+				break;
+			case 'h':
+			case '?':
+				System.out.println(help);
+				System.exit(0);
+				break; // getopt() already printed an error
+			}
+		}
+
+		if (outputDir.equals("") || edgeType.equals("") || cutoff==0.0) {
+			System.err.println("Some missing option");
+			System.err.println(help);
+			System.exit(1);
+		}
+		if (listfile.equals("") && pdbCodes==null && pdbfile.equals("")){
+			System.err.println("Either a listfile, some pdb codes/chain codes or a pdbfile must be given");
+			System.err.println(help);
+			System.exit(1);
+		}
+		if ((!listfile.equals("") && pdbCodes!=null) || (!listfile.equals("") && !pdbfile.equals("")) || (pdbCodes!=null && !pdbfile.equals(""))) {
+			System.err.println("Options -p/-c, -i and -f/-c are exclusive. Use only one of them");
+			System.err.println(help);
+			System.exit(1);			
+		}
+
+		
+		MySQLConnection conn = null;		
+
+		try{
+			conn = new MySQLConnection(DB_HOST, DB_USER, DB_PWD);
+		} catch (Exception e) {
+			System.err.println("Error opening database connection. Exiting");
+			System.exit(1);
+		}
+		
+		
+		if (pdbfile.equals("")){
+			
+			if (!listfile.equals("")) {			
+				BufferedReader fpdb = new BufferedReader(new FileReader(listfile));
+				String line = "";
+				int numLines = 0;
+				fpdb.mark(100000);
+				while ((line = fpdb.readLine() ) != null ) {
+					if (line.length()>0) numLines++;
+				}
+				fpdb.reset();
+				pdbCodes = new String[numLines];
+				pdbChainCodes = new String[numLines];
+				numLines = 0;
+				while ((line = fpdb.readLine() ) != null ) {
+					pdbCodes[numLines] = line.split("\\s+")[0].toLowerCase();
+					pdbChainCodes[numLines] = line.split("\\s+")[1];
+					numLines++;
+				}
+			}
+
+			int numPdbs = 0;
+
+			for (int i=0;i<pdbCodes.length;i++) {
+				String pdbCode = pdbCodes[i];
+				String pdbChainCode = pdbChainCodes[i];
+
+				try {
+					
+					long start = System.currentTimeMillis();
+					
+					Pdb pdb = new PdbasePdb(pdbCode, pdbaseDb, conn);
+					pdb.load(pdbChainCode);
+
+					// get graph
+					RIGraph graph = pdb.get_graph(edgeType, cutoff);
+					
+					String edgeTypeStr = edgeType.replaceAll("/", ":");
+					
+					File outputFile = new File(outputDir,pdbCode+"_"+pdbChainCode+"_"+edgeTypeStr+"_"+cutoff+".graph");
+					graph.write_graph_to_file(outputFile.getAbsolutePath());
+
+					long end = System.currentTimeMillis();
+					double time = (double) (end -start)/1000;
+
+					System.out.println("Wrote "+outputFile.getAbsolutePath());
+					System.out.printf("%5.3f s\n",time);
+					
+					numPdbs++;
+
+				} catch (PdbLoadError e) {
+					System.err.println("Error loading pdb data for " + pdbCode + pdbChainCode+", specific error: "+e.getMessage());
+				} catch (PdbCodeNotFoundError e) {
+					System.err.println("Couldn't find pdb code "+pdbCode);
+				} catch (SQLException e) {
+					System.err.println("SQL error for structure "+pdbCode+"_"+pdbChainCode+", error: "+e.getMessage());
+				}
+
+			}
+
+			// output results
+			System.out.println("Number of structures done successfully: " + numPdbs);
+
+
+		} else {
+			String pdbChainCode = pdbChainCodes[0];
+			try {
+				Pdb pdb = new PdbfilePdb(pdbfile);
+				pdb.load(pdbChainCode);
+				if (!pdb.hasSecondaryStructure()) {
+					pdb.runDssp(DSSP_EXE, DSSP_PARAMS);
+				}
+				RIGraph graph = pdb.get_graph(edgeType, cutoff);
+
+				String edgeTypeStr = edgeType.replaceAll("/", ":");
+				
+				File outputFile = new File(outputDir,pdb.getPdbCode()+"_"+pdbChainCode+"_"+edgeTypeStr+"_"+cutoff+".graph");
+				graph.write_graph_to_file(outputFile.getAbsolutePath());
+				System.out.println("Wrote graph file "+outputFile.getAbsolutePath()+" from pdb file "+pdbfile);
+				
+			} catch (PdbLoadError e) {
+				System.err.println("Error loading from pdb file "+pdbfile+", specific error: "+e.getMessage());
+			}
+		}
+	}
+
+}
---
+++ aglappe-0.9/tinker/PRMInfo.java
@@ -0,0 +1,140 @@
+package tinker;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import proteinstructure.AAinfo;
+
+/**
+ * Class containing information (atom identities) parsed from 
+ * tinker's prm files (force field parameter sets)
+ *
+ */
+public class PRMInfo {
+
+	String prmFileName;
+	String type;
+	
+	TreeMap<Integer,Integer> prmid2tinkerid;
+	TreeMap<Integer,String> prmid2prmname;
+	TreeMap<Integer,String> prmid2fullname;
+	
+	TreeMap<Integer,String> prmid2pdbname;
+	
+	TreeMap<Integer,String> prmid2res_atom;
+	
+	public PRMInfo(String prmFileName, String type) throws IOException{
+		this.prmFileName = prmFileName;
+		this.type = type;
+		
+		readPrmFile();
+		
+		if (type.equals("amber")){
+			mapPdbAtomNamesAmber();
+		} else {
+			System.err.println("prm file type "+type+" not supported");
+		}
+	}
+	
+	private void readPrmFile() throws IOException{
+		prmid2tinkerid = new TreeMap<Integer, Integer>();
+		prmid2prmname = new TreeMap<Integer, String>();
+		prmid2fullname = new TreeMap<Integer, String>();
+		
+		
+		BufferedReader fprm = new BufferedReader(new FileReader(prmFileName));
+		String line;
+		while((line = fprm.readLine()) != null ) {
+			//atom      1    14    N       "Glycine N"                 7     14.010     3
+			Pattern p = Pattern.compile("^atom\\s+(\\d+)\\s+(\\d+)\\s+([a-zA-Z0-9*]+)\\s+\"([^\"]+)\""); // note for atom names we use [a-zA-Z0-9*] instead of \w (because * also appears in some atom names)
+			Matcher m = p.matcher(line);
+			if (m.find()){
+				int prmId = Integer.parseInt(m.group(1));
+				int tinkerId = Integer.parseInt(m.group(2));
+				String prmName = m.group(3);
+				String fullName = m.group(4);
+				prmid2tinkerid.put(prmId, tinkerId);
+				prmid2prmname.put(prmId,prmName);
+				prmid2fullname.put(prmId,fullName);
+			}
+		}
+		fprm.close();
+	}
+	
+	private void mapPdbAtomNamesAmber() {
+		//NOTE amber uses some special atom names as compared to pdb:
+		//					pdb			amber
+		// ARG:				NH1,NH2		NH  	atoms are indistinguishable
+		// GLU:				OE1,OE2		OE		atoms are indistinguishable
+		// PHE: 			CD1,CD2		CD		atoms are indistinguishable
+		// 					CE1,CE2		CE		atoms are indistinguishable
+		// TYR: 			CD1,CD2		CD		atoms are indistinguishable
+		//					CE1,CE2		CE		atoms are indistinguishable
+		// ILE:				CD1			CD		change of nomenclature
+		// ASP: 			OD1,OD2		OD		they don't seem to be indistiguishable, why do they use the same name??
+		// all c-term aas:	O, OXT		OXT		they don't seem to be indistiguishable, why do they use the same name??
+
+		prmid2res_atom = new TreeMap<Integer, String>();
+		
+		for (int prmid:prmid2fullname.keySet()){
+			String fullname = prmid2fullname.get(prmid);
+
+			String molName = fullname.substring(0, fullname.lastIndexOf(" ")).trim();
+			molName = molName.replace("C-Term ", "");
+			molName = molName.replace("N-Term ", "");
+			String atomName = fullname.substring(fullname.lastIndexOf(" ")).trim();
+			// amber uses OXT as the atom name for BOTH Oxygens in the mainchain of the C-Term aminoacid
+			// we don't want OXTs so here we replace it by O which is the symbol for the usual backbone Oxygen
+			// After when we map xyz to pdb atom serials, the first amber OXT type atom encountered in the c-term aminoacid of the xyz file will be considered as the backbone Oxygen
+			if (atomName.equals("OXT")) {
+				atomName = "O";
+			}
+			
+			for (String name:AAinfo.getAAFullNames()) {
+				// here we can't just call AAinfo.isValidFullName as we test only if the string STARTS like a full amino acid name
+				// this is because some aminoacid names are 1 word and some 2 words (e.g. Aspartic Acid) 
+				if(molName.startsWith(name)) molName=AAinfo.fullname2threeletter(name);
+			}
+			
+			if (AAinfo.isValidAA(molName.substring(0, 3))) {
+				// Hydrogens will also be in the mapping, in case we need them later
+				prmid2res_atom.put(prmid, molName.substring(0,3)+"_"+atomName);
+			}
+
+		}		
+		
+	}
+	
+	public String getRes_AtomFromPrmid(int prmid) {
+		return prmid2res_atom.get(prmid);
+	}
+	
+	/*
+	"Alanine"
+	"Arginine"
+	"Asparagine"
+	"Aspartic Acid"
+	"Cysteine (-SH)"
+	"Glutamic Acid"
+	"Glutamine"
+	"Glycine"
+	"Histidine (+)"
+	"Histidine (HD)"
+	"Histidine (HE)"
+	"Isoleucine"
+	"Leucine"
+	"Lysine"
+	"Methionine"
+	"Phenylalanine"
+	"Proline"
+	"Serine"
+	"Threonine"
+	"Tryptophan"
+	"Tyrosine"
+	"Valine"
+*/
+}
