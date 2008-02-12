7e38981abc181652b75f44abe64221db9c01d722
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2008-02-12 11:50:05 +0000

    New constructor to read from simple graph files, i.e. list of contacts without any headers

--- proteinstructure/FileRIGraph.java
+++ proteinstructure/FileRIGraph.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.TreeMap;
@@ -23,15 +24,30 @@ public class FileRIGraph extends RIGraph {
 
 	private static double DEFAULT_WEIGHT = 1.0;
 	
+	private boolean simple;
+	
 	/**
-	 * Constructs RIGraph object by reading a file with contacts
+	 * Constructs RIGraph object by reading a file with contacts in aglappe format
 	 * If the contacts file doesn't have the sequence then the RIGraph object won't have sequence or residue types in RIGNodes
 	 * @param contactsfile
 	 * @throws IOException
 	 * @throws GraphFileFormatError
 	 */
 	public FileRIGraph (String contactsfile) throws IOException, GraphFileFormatError{
+		this(contactsfile, false);
+	}
+
+	/**
+	 * Constructs RIGraph object by reading a file with contacts
+	 * If the contacts file doesn't have the sequence then the RIGraph object won't have sequence or residue types in RIGNodes
+	 * @param contactsfile
+	 * @param simple true if we want to read file in "simple" format, i.e. only a list of edges, false if file in aglappe format
+	 * @throws IOException
+	 * @throws GraphFileFormatError
+	 */
+	public FileRIGraph (String contactsfile, boolean simple) throws IOException, GraphFileFormatError{
 		super();
+		this.simple = simple;
 		this.contactType=ProtStructGraph.NO_CONTACT_TYPE;
 		this.distCutoff=ProtStructGraph.NO_CUTOFF;
 		// we initialise pdbCode, chainCode and pdbChainCode to corresponding constants (empty strings at the moment) in case the file doesn't specify then
@@ -42,7 +58,7 @@ public class FileRIGraph extends RIGraph {
 		read_graph_from_file(contactsfile);  
 		
 	}
-
+	
 	/**
 	 * Parses the graph file reading identifiers, sequence and edges
 	 * @param contactsfile
@@ -58,47 +74,48 @@ public class FileRIGraph extends RIGraph {
 		String line;
 		while ((line = fcont.readLine() ) != null ) {
 			linecount++;
-			Pattern p = Pattern.compile("^#AGLAPPE.*ver: (\\d\\.\\d)");
-			Matcher m = p.matcher(line);
-			if (m.find()){
-				if (!m.group(1).equals(GRAPHFILEFORMATVERSION)){
-					throw new GraphFileFormatError(contactsfile+" has a wrong file format version. Supported version is "+GRAPHFILEFORMATVERSION+" and found version was "+m.group(1));
+			if (!simple) {
+				Pattern p = Pattern.compile("^#AGLAPPE.*ver: (\\d\\.\\d)");
+				Matcher m = p.matcher(line);
+				if (m.find()){
+					if (!m.group(1).equals(GRAPHFILEFORMATVERSION)){
+						throw new GraphFileFormatError(contactsfile+" has a wrong file format version. Supported version is "+GRAPHFILEFORMATVERSION+" and found version was "+m.group(1));
+					}
+				} else if (linecount==1){ // #AGLAPPE not found and in first line
+					throw new GraphFileFormatError(contactsfile+" is not a valid contact map file");
 				}
-			} else if (linecount==1){ // #AGLAPPE not found and in first line
-				throw new GraphFileFormatError(contactsfile+" is not a valid contact map file");
+				Pattern ps = Pattern.compile("^#SEQUENCE:\\s*(\\w+)$");
+				Matcher ms = ps.matcher(line);
+				if (ms.find()){
+					sequence=ms.group(1);
+				}
+				ps = Pattern.compile("^#PDB:\\s*(\\w+)");
+				ms = ps.matcher(line);
+				if (ms.find()){
+					pdbCode=ms.group(1);
+				}
+				ps = Pattern.compile("^#PDB CHAIN CODE:\\s*(\\w+)");
+				ms = ps.matcher(line);
+				if (ms.find()){
+					pdbChainCode=ms.group(1);
+				}
+				ps = Pattern.compile("^#CHAIN:\\s*(\\w)");
+				ms = ps.matcher(line);
+				if (ms.find()){
+					chainCode=ms.group(1);
+				}				
+				ps = Pattern.compile("^#CT:\\s*([a-zA-Z/]+)");
+				ms = ps.matcher(line);
+				if (ms.find()){
+					contactType=ms.group(1);
+				}												
+				ps = Pattern.compile("^#CUTOFF:\\s*(\\d+\\.\\d+)");
+				ms = ps.matcher(line);
+				if (ms.find()){
+					distCutoff=Double.parseDouble(ms.group(1));
+				}								
 			}
-			Pattern ps = Pattern.compile("^#SEQUENCE:\\s*(\\w+)$");
-			Matcher ms = ps.matcher(line);
-			if (ms.find()){
-				sequence=ms.group(1);
-			}
-			ps = Pattern.compile("^#PDB:\\s*(\\w+)");
-			ms = ps.matcher(line);
-			if (ms.find()){
-				pdbCode=ms.group(1);
-			}
-			ps = Pattern.compile("^#PDB CHAIN CODE:\\s*(\\w+)");
-			ms = ps.matcher(line);
-			if (ms.find()){
-				pdbChainCode=ms.group(1);
-			}
-			ps = Pattern.compile("^#CHAIN:\\s*(\\w)");
-			ms = ps.matcher(line);
-			if (ms.find()){
-				chainCode=ms.group(1);
-			}				
-			ps = Pattern.compile("^#CT:\\s*([a-zA-Z/]+)");
-			ms = ps.matcher(line);
-			if (ms.find()){
-				contactType=ms.group(1);
-			}												
-			ps = Pattern.compile("^#CUTOFF:\\s*(\\d+\\.\\d+)");
-			ms = ps.matcher(line);
-			if (ms.find()){
-				distCutoff=Double.parseDouble(ms.group(1));
-			}								
-
-			Pattern pcontact = Pattern.compile("^\\s*(\\d+)\\s+(\\d+)(?:\\s+(\\d+\\.\\d+))?\\s*$");
+			Pattern pcontact = Pattern.compile("^\\s*(\\d+)\\s+(\\d+)(?:\\s+(\\d+(?:\\.\\d+)?))?\\s*$");
 			Matcher mcontact = pcontact.matcher(line);
 			if (mcontact.find()){
 				int i = Integer.valueOf(mcontact.group(1));
@@ -114,6 +131,14 @@ public class FileRIGraph extends RIGraph {
 
 		}
 		fcont.close();
+		
+		if (simple) {
+			int maxResSer = Collections.max(allserials);
+			sequence = "";
+			for (int resser=1;resser<=maxResSer;resser++) {
+				sequence+=AAinfo.UNKNOWN_UNOBSERVED_RES_ONE_LETTER;
+			}
+		}
 		
 		// if we didn't find a sequence we throw format exception
 		if (sequence==null) {
