cac6fce5ad929413dfb69e0e8e5c6d662354a5bb
Henning Stehr <h.stehr@gmail.com>
2008-04-22 09:06:57 +0000

    removed import which caused a warning

--- proteinstructure/RIGraph.java
+++ proteinstructure/RIGraph.java
@@ -7,7 +7,6 @@ import java.io.PrintStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Locale;
