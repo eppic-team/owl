a98f4ab55d461f2192ddbdd5cbb23fff42384b73
Henning Stehr <h.stehr@gmail.com>
2008-05-22 12:18:31 +0000

    Method sortByValue now called sortMapByValue and takes an additional argument for the sorting order (ascening/descending)

--- tools/Goodies.java
+++ tools/Goodies.java
@@ -8,18 +8,30 @@ import java.util.*;
  */
 public class Goodies {
 	
+	public static final boolean ASCENDING = true;
+	public static final boolean DESCENDING = false;	
+	
 	/**
 	 * Sorts a map by the values and returns a sorted map with the right ordering.
 	 * @param map the initial map
+	 * @param ascending if true, map will be sorted in ascending order of values, otherwise in descending order
 	 * @return the sorted map
 	 */
-	public static <K,V extends Comparable<V>> Map<K,V> sortByValue(Map<K,V> map) {
+	public static <K,V extends Comparable<V>> Map<K,V> sortMapByValue(Map<K,V> map, boolean ascending) {
 		List<Map.Entry<K,V>> list = new LinkedList<Map.Entry<K,V>>(map.entrySet());
+		if(ascending) {
 		Collections.sort(list, new Comparator<Map.Entry<K,V>>() {
 			public int compare(Map.Entry<K,V> o1, Map.Entry<K,V> o2) {
-				return ((Comparable<V>) o1.getValue()).compareTo(o2.getValue());				
+				return (o1.getValue()).compareTo(o2.getValue());				
 			}
 		});
+		} else {
+			Collections.sort(list, new Comparator<Map.Entry<K,V>>() {
+				public int compare(Map.Entry<K,V> o1, Map.Entry<K,V> o2) {
+					return -(o1.getValue()).compareTo(o2.getValue());				
+				}
+			});			
+		}
 
 		Map<K,V> result = new LinkedHashMap<K,V>();
 		for (Iterator<Map.Entry<K,V>> it = list.iterator(); it.hasNext();) {
@@ -28,5 +40,6 @@ public class Goodies {
 		}
 		return result;
 	}
-
+	
+		
 }
