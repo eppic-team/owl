ec10d54ee7f754bd0a7aa2e28e79877b1f8cbc99
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2008-08-08 13:07:02 +0000

    Now reconstruction from contact map files also possible. Improvements to help text and error checks.

--- reconstruct.java
+++ reconstruct.java
@@ -1,10 +1,12 @@
 import gnu.getopt.Getopt;
 import graphAveraging.ConsensusSquare;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.Formatter;
@@ -15,6 +17,8 @@ import java.util.regex.Pattern;
 
 import proteinstructure.AAinfo;
 import proteinstructure.ConformationsNotSameSizeError;
+import proteinstructure.FileRIGraph;
+import proteinstructure.GraphFileFormatError;
 import proteinstructure.PdbLoadError;
 import proteinstructure.RIGraph;
 import proteinstructure.Pdb;
@@ -23,16 +27,23 @@ import proteinstructure.PdbasePdb;
 
 import tinker.TinkerError;
 import tinker.TinkerRunner;
+import tools.MySQLConnection;
 
 
 public class reconstruct {
 
 	private static final String TINKERBINDIR = "/project/StruPPi/Software/tinker/bin";
 	private static final String PRMFILE = "/project/StruPPi/Software/tinker/amber/amber99.prm";
+	
+	private static final String MYSQLSERVER = "white";
+	private static final String MYSQLUSER = MySQLConnection.getUserName();
+	private static final String MYSQLPWD = "nieve";
+	private static final String PDBASEDB = "pdbase";
 
 	private static final double DEFAULT_FORCECONSTANT_DISTANCE = TinkerRunner.DEFAULT_FORCECONSTANT_DISTANCE;
 	private static final String DEFAULT_CONTACT_TYPE = "Cb";
 	private static final double DEFAULT_CUTOFF = 8.0;
+	private static final String DEFAULT_BASENAME = "rec";
 	
 	private static final TreeMap<Integer, ConsensusSquare> NO_PHIPSI_CONSTRAINTS = null;
 	private static final boolean NO_OMEGA_CONSTRAINTS = false;
@@ -44,63 +55,61 @@ public class reconstruct {
 
 		String help = "Reconstructs a protein structure from a contact map using tinker's distgeom.\n" +
 			" Two modes of operation:\n" +
-			"  a) normal      : NOT IMPLEMENTED YET. specify a sequence and one or more contact map files\n" +
-			"  b) benchmarking: specify a pdb code + pdb chain code (-p)\n" +
+			"  a) normal      : specify one or more contact map files. The sequence, contact \n" +
+			"                   type and cutoff will be taken from the file\n" +
+			"  b) benchmarking: specify a pdb code + pdb chain code (-p) and optionally \n" +
+			"                   contact type (-t) and cutoff (-d)\n" +
 			"Usage:\n" +
-			PROG_NAME+"\n"+
+			PROG_NAME+" [options] [contact_map_file_1 [contact_map_file_2] [...]] \n"+
 			"  -p <string>   : pdb code + pdb chain code, e.g. 1abcA (benchmarking)\n" +
-			"  -b <string>   : base name of output files \n" +
-			" [-t <string>]  : contact type. Either 1 contact type: Cb or 2 separated by \n" +
-			"                  underscore: Ca_Cg, default: "+DEFAULT_CONTACT_TYPE+" \n" +
-			" [-r]           : cross. Use also the cross contact type of the 2 contact types \n" +
-			"                  specified. e.g. if Ca_Cg given in -t and -r specified then Ca/Cg \n" +
-			"                  contact map will also be used to reconstruct\n" +
-			" [-d <floats>]  : distance cutoff(s) comma separated, specify up to 3 distance cutoffs:\n" +
-			"                  1st for 1st contact type, 2nd for 2nd contact type 3rd for crossed contact type\n" +
-			"                  If 1 specified then it will used for all contact types. Default: "+DEFAULT_CUTOFF+" \n" +
-			" [-o <dir>]     : output dir, default: current \n" +
-			" [-n <int>]     : number of models to generate, default: 1 \n" +
-			" [-m <int>]     : min range, default: 0 \n" +
-			" [-M <int>]     : max range, default: 0 \n" +
+			" [-t <string>]  : one or more contact types comma separated (benchmarking). Default: "+DEFAULT_CONTACT_TYPE+" \n" +
+			" [-d <floats>]  : one or more distance cutoffs comma separated (benchmarking), \n" +
+			"                  matching given contact types. If only one specified then it will \n" +
+			"                  used for all contact types. Default: "+DEFAULT_CUTOFF+" \n" +
+			"\n" +
+			" [-b <string>]  : base name of output files. Default: "+DEFAULT_BASENAME+" or pdbId given in -p\n" +
+			" [-o <dir>]     : output dir. Default: current \n" +
+			" [-n <int>]     : number of models to generate. Default: 1 \n" +
+			" [-m <int>]     : filter contacts to min range. Default: no filtering \n" +
+			" [-M <int>]     : filter contacts to max range. Default: no filtering \n" +
 			" [-f <float>]   : force constant, default: "+DEFAULT_FORCECONSTANT_DISTANCE+" \n" +
 			" [-F]           : fast mode: refinement will be done via minimization (faster but \n" +
-			"                  worse quality model). Default: slow (refinement via simulated annealing) \n\n";
+			"                  worse quality model). Default: slow (refinement via simulate\n" +
+			"                  annealing) \n\n";
 
+		boolean benchmark = true;
 		String pdbId = null;
 		String pdbCode = null;
 		String pdbChainCode = null;
-		String ct = DEFAULT_CONTACT_TYPE;
-		String[] cutoffs = null;
-		double cutoff1 = DEFAULT_CUTOFF;
-		double cutoff2 = 0.0;
-		double cutoff3 = 0.0;
+		String[] cts = {DEFAULT_CONTACT_TYPE};
+		double[] cutoffs = {DEFAULT_CUTOFF};
+		File[] cmFiles = null;
 		String outputDir = "."; //default current
 		String baseName = null;
-		boolean cross = false;
 		int n = 1;
 		double forceConstant = DEFAULT_FORCECONSTANT_DISTANCE; 
 		int minRange = 0;
 		int maxRange = 0;
 		boolean fast = false;
 		
-		Getopt g = new Getopt(PROG_NAME, args, "p:t:rd:b:o:n:m:M:f:Fh?");
+		Getopt g = new Getopt(PROG_NAME, args, "p:b:t:d:o:n:m:M:f:Fh?");
 		int c;
 		while ((c = g.getopt()) != -1) {
 			switch(c){
 			case 'p':
 				pdbId = g.getOptarg();
 				break;
-			case 't':
-				ct = g.getOptarg();
-				break;
-			case 'r':
-				cross = true;
-				break;
-			case 'd':
-				cutoffs = g.getOptarg().split(",");
-				break;
 			case 'b':
 				baseName = g.getOptarg();
+				break;				
+			case 't':
+				cts = g.getOptarg().split(",");
+				break;
+			case 'd':
+				String[] tokens = g.getOptarg().split(",");
+				cutoffs=new double[tokens.length];
+				for (int i=0;i<tokens.length;i++) 
+					cutoffs[i]=Double.parseDouble(tokens[i]);
 				break;
 			case 'o':
 				outputDir = g.getOptarg();
@@ -127,130 +136,178 @@ public class reconstruct {
 				break; // getopt() already printed an error
 			}
 		}
-
-		if (pdbId==null || baseName==null){
-			System.err.println("Must specify at least -p and -b");
+		// parsing non-option elements (contact map files)
+		if (args.length-g.getOptind()>0) {
+			cmFiles = new File[args.length-g.getOptind()];
+			for (int index=g.getOptind();index<args.length;index++) {
+				cmFiles[index-g.getOptind()]= new File(args[index]);
+				benchmark = false;
+			}
+		}
+		
+		// input checks
+		if (pdbId==null && cmFiles == null) {
+			System.err.println("Either a pdb id (-p) or at least one contact map file must be given");
 			System.err.println(help);
 			System.exit(1);
 		}
-		
-		if (baseName.contains(".")) {
-			System.err.println("Basename can't contain a dot (not allowed by tinker). Exiting");
-			System.exit(1);
-		}
-		
+				
 		if (n>999) {
 			System.err.println("Maximum number of models is 999. Specify a lower value. Exiting");
 			System.exit(1);
 		}
 		
-		// pdb code and chain code
-		Pattern p = Pattern.compile("(\\d\\w\\w\\w)(\\w)");
-		Matcher m = p.matcher(pdbId);
-		if (m.matches()) {
-			pdbCode = m.group(1);
-			pdbChainCode = m.group(2);
-		} else {
-			System.err.println("PDB id given not in the correct format, must be in the form 1abcA");
+		if (benchmark && cts.length!=cutoffs.length && cutoffs.length!=1) {
+			System.err.println("The number of contact types and cutoffs given must be the same. With the exception of only 1 cutoff given that will be taken for all contact types");
 			System.exit(1);
-		}
-		
-		// cutoffs assignment
-		if (cutoffs!=null) {
-			cutoff1 = Double.parseDouble(cutoffs[0]);
-			cutoff2 = cutoff1;
-			cutoff3 = cutoff1;
-			if (cutoffs.length>1) {
-				cutoff2 = Double.parseDouble(cutoffs[1]);
-				if (cutoffs.length>2) {
-					cutoff3 = Double.parseDouble(cutoffs[2]);
-				}
-			}
 		}
 
-		// contact type assignment
-		boolean doublecm = false;
-		String ct1 = ct;
-		String ct2 = ct;
-		String ct3 = null;
-		if (ct.contains("_")) {
-			ct1 = ct.split("_")[0];
-			ct2 = ct.split("_")[1];
-			doublecm = true;
+		if (benchmark) {
+			// assigning default cutoff if only 1 given and multi ct given
+			if (cts.length>1 && cutoffs.length==1) {
+				double cutoff = cutoffs[0];
+				cutoffs = new double[cts.length];
+				for (int i=0;i<cts.length;i++) 
+					cutoffs[i] = cutoff;
+			}
 		}
+		if (pdbId!=null) {
+			// pdb code and chain code
+			Pattern p = Pattern.compile("(\\d\\w\\w\\w)(\\w)");
+			Matcher m = p.matcher(pdbId);
+			if (m.matches()) {
+				pdbCode = m.group(1);
+				pdbChainCode = m.group(2);
+			} else {
+				System.err.println("PDB id given not in the correct format, must be in the form 1abcA");
+				System.exit(1);
+			}		
+		}		
 		
-		if (!AAinfo.isValidContactType(ct1) || !AAinfo.isValidContactType(ct2) || ct.contains("ALL")) {
-			System.err.println("Invalid contact type specified. Exiting");
+		// setting a default for baseName if it wasn't specified and checking basename
+		if (baseName==null) {
+			if (benchmark) baseName=pdbCode+pdbChainCode;
+			else baseName = DEFAULT_BASENAME;
+		}		
+		if (baseName.contains(".")) { 
+			System.err.println("Basename can't contain a dot (not allowed by tinker). Exiting");
 			System.exit(1);
+		}	
+		// checking a baseName.pdb file doesn't exist in outdir 
+		if (new File(outputDir,baseName+".pdb").exists()) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("A file baseName.pdb ("+baseName+".pdb) exists in the output directory. File will be overwritten. Continue (y/n)? ");
+			try {
+				String answer = br.readLine();
+				if (!answer.equals("y")) {
+					System.exit(1);
+				}
+			} catch (IOException e) {
+				System.err.println("Error while trying to read standard input");
+				System.exit(1);
+			}
 		}
-		
-		if (cross) {
-			ct3 = ct1+"/"+ct2;
-		}
-		
-		
+				
+		// getting pdb data if a pdbId was specified 
 		Pdb pdb = null;
 		Pdb mPdb = null;
-		try {
-			pdb = new PdbasePdb(pdbCode);
-			pdb.load(pdbChainCode);
-			mPdb = new PdbasePdb(pdbCode);
-			mPdb.load(pdbChainCode);
-			mPdb.mirror();
-		} catch (PdbLoadError e) {
-			System.err.println("Error while loading pdb data. Specific error "+e.getMessage());
-			System.exit(1);
-		} catch (PdbCodeNotFoundError e) {
-			System.err.println("Given pdb code "+pdbCode+" couldn't be found in pdbase. Exiting");
-			System.exit(1);
-		} catch (SQLException e) {
-			System.err.println("Problems connecting to database for getting pdb data for "+pdbCode+". Exiting");
-			System.exit(1);
-		}
-		// we also write the file to the out dir so it can be used later for clustering rmsds etc.
-		File origPdbFile = new File (outputDir,baseName+".native.pdb");
-		try {
-			pdb.dump2pdbfile(origPdbFile.getAbsolutePath());
-		} catch (IOException e4) {
-			System.err.println("Couldn't write original pdb file "+origPdbFile.getAbsolutePath());
-			System.err.println("Continuing without it, this is not needed for the rest of the reconstruction process but only for post processing (e.g. comparing rmsds to original)");
-		}
+		String sequence = null;
+		File origPdbFile = null;
+		if (pdbId!=null) {
+			try {
+				MySQLConnection conn = new MySQLConnection(MYSQLSERVER, MYSQLUSER, MYSQLPWD);
+				pdb = new PdbasePdb(pdbCode, PDBASEDB, conn);
+				pdb.load(pdbChainCode);
+				mPdb = new PdbasePdb(pdbCode, PDBASEDB, conn);
+				mPdb.load(pdbChainCode);
+				mPdb.mirror();
+			} catch (PdbLoadError e) {
+				System.err.println("Error while loading pdb data. Specific error "+e.getMessage());
+				System.exit(1);
+			} catch (PdbCodeNotFoundError e) {
+				System.err.println("Given pdb code "+pdbCode+" couldn't be found in pdbase. Exiting");
+				System.exit(1);
+			} catch (SQLException e) {
+				System.err.println("Problems connecting to database for getting pdb data for "+pdbCode+". Exiting");
+				System.exit(1);
+			}
+			// we also write the file to the out dir so it can be used later for clustering rmsds etc.
+			origPdbFile = new File (outputDir,baseName+".native.pdb");
+			try {
+				pdb.dump2pdbfile(origPdbFile.getAbsolutePath());
+			} catch (IOException e4) {
+				System.err.println("Couldn't write original pdb file "+origPdbFile.getAbsolutePath());
+				System.err.println("Continuing without it, this is not needed for the rest of the reconstruction process but only for post processing (e.g. comparing rmsds to original)");
+			}
+			
+			sequence = pdb.getSequence();
+		} 
 
-		String sequence = pdb.getSequence();
+		
 
-		RIGraph[] graphs =null;
-		RIGraph graph1 = pdb.get_graph(ct1, cutoff1);
-		if (maxRange>0) graph1.restrictContactsToMaxRange(maxRange);
-		if (minRange>0) graph1.restrictContactsToMinRange(minRange);
-		RIGraph graph2 = null;
-		RIGraph graph3 = null;
-		if (doublecm) {
-			graph2 = pdb.get_graph(ct2, cutoff2);
-			if (maxRange>0) graph2.restrictContactsToMaxRange(maxRange);
-			if (minRange>0) graph2.restrictContactsToMinRange(minRange);
-			if (cross) {
-				graph3 = pdb.get_graph(ct3, cutoff3);
-				if (maxRange>0) graph3.restrictContactsToMaxRange(maxRange);
-				if (minRange>0) graph3.restrictContactsToMinRange(minRange);
-				graphs = new RIGraph[3];
-				graphs[0] = graph1;
-				graphs[1] = graph2;
-				graphs[2] = graph3;
-			} else {
-				graphs = new RIGraph[2];
-				graphs[0] = graph1;
-				graphs[1] = graph2;
+		// initialising contact maps
+		RIGraph[] graphs = new RIGraph[cts.length];
+		
+		if (benchmark) {
+			for (int i=0;i<cts.length;i++) {
+				graphs[i] = pdb.get_graph(cts[i], cutoffs[i]);
 			}
 		} else {
-			graphs = new RIGraph[1];
-			graphs[0] = graph1;
+			graphs = new RIGraph[cmFiles.length];
+			cts = new String[cmFiles.length];
+			cutoffs = new double[cmFiles.length];
+			try {
+				for (int i=0;i<cmFiles.length;i++) {
+					graphs[i]=new FileRIGraph(cmFiles[i].getAbsolutePath());
+					cts[i] = graphs[i].getContactType();
+					cutoffs[i] = graphs[i].getCutoff();
+					if (cutoffs[i]==0) {
+						System.err.println("Contact Map file "+cmFiles[i]+" has no cutoff. Can't continue.");
+						System.exit(1);
+					}
+				}
+				if (graphs[0].hasSequence()) {
+					sequence = graphs[0].getSequence();
+				} else {
+					System.err.println("Contact Map file "+cmFiles[0]+" has no sequence. Can't continue.");
+					System.exit(1);
+				}
+				if (graphs.length>1) {
+					for (int i=1;i<graphs.length;i++) {
+						if (!graphs[i].getSequence().equals(sequence)){
+							System.err.println("Sequence of contact map file "+cmFiles[i]+" doesn't coincide with sequence of file "+cmFiles[0]);
+							System.exit(1);
+						}
+					}
+				}
+			} catch (GraphFileFormatError e) {
+				System.err.println("Contact Map file has wrong formatting: "+e.getMessage());
+				System.exit(1);
+			} catch (IOException e) {
+				System.err.println("Couldn't read contact map file: " +e.getMessage());
+				System.exit(1);
+			}
+			
 		}
 		
-		// defining files
+		// checking contact types
+		for (String ct: cts) {
+			if (!AAinfo.isValidContactType(ct) || ct.contains("ALL")) {
+				System.err.println("Invalid contact type specified. Exiting");
+				System.exit(1);
+			}
+		}
+		
+		// restricting to max/min range
+		for (RIGraph graph:graphs) {
+			if (maxRange>0) graph.restrictContactsToMaxRange(maxRange);
+			if (minRange>0) graph.restrictContactsToMinRange(minRange);			
+		}
+
+		// defining report file
 		File reportFile = new File(outputDir,baseName+".report");
 		
-		// creating TinkerRunner object
-		
+		// creating TinkerRunner object		
 		TinkerRunner tr = null;
 		try {
 			tr = new TinkerRunner(TINKERBINDIR, PRMFILE); 
@@ -259,8 +316,7 @@ public class reconstruct {
 			System.exit(1);
 		}
 		
-		// call reconstruction
-		
+		// call reconstruction		
 		try {
 			if (fast) {
 				// as this is at the moment just a reconstruction benchmarking script it doesn't make sense 
@@ -296,21 +352,22 @@ public class reconstruct {
 		double[] rmsds = new double[n+1];		
 		double[] mRmsds = new double[n+1];		
 		
-		for (int i = 1; i<=n; i++) {
-			String ext = new Formatter().format(".%03d",i).toString();
-			File outputPdbFile = new File(outputDir, baseName+ext+".pdb");
-			try {
-				Pdb outputPdb = tr.getStructure(i);
-				rmsds[i] = pdb.rmsd(outputPdb, "Ca");
-				mRmsds[i] = mPdb.rmsd(outputPdb, "Ca");
-			}
-			catch (TinkerError e) {
-				System.err.println("Error while trying to retrieve results from Tinker: "+ e.getMessage());
-			} catch (ConformationsNotSameSizeError e) {
-				System.err.println(origPdbFile.getAbsolutePath()+" and "+outputPdbFile.getAbsolutePath()+" don't have the same conformation size, can't calculate rmsd for them.");
-			}				
-		}					
-
+		if (pdbId!=null) {
+			for (int i = 1; i<=n; i++) {
+				String ext = new Formatter().format(".%03d",i).toString();
+				File outputPdbFile = new File(outputDir, baseName+ext+".pdb");
+				try {
+					Pdb outputPdb = tr.getStructure(i);
+					rmsds[i] = pdb.rmsd(outputPdb, "Ca");
+					mRmsds[i] = mPdb.rmsd(outputPdb, "Ca");
+				}
+				catch (TinkerError e) {
+					System.err.println("Error while trying to retrieve results from Tinker: "+ e.getMessage());
+				} catch (ConformationsNotSameSizeError e) {
+					System.err.println(origPdbFile.getAbsolutePath()+" and "+outputPdbFile.getAbsolutePath()+" don't have the same conformation size, can't calculate rmsd for them.");
+				}				
+			}					
+		}
 		// write report file
 		
 		try {
@@ -324,8 +381,20 @@ public class reconstruct {
 				String rmsd = String.format(Locale.US,"%6.3f",rmsds[i]);
 				String mRmsd = String.format(Locale.US,"%6.3f",mRmsds[i]);
 				String errStr = String.format(Locale.US, "%6.3f", err[i]);
-				//                     run_id                cutoff      cutoff2      cutoff3      ct1      ct2      ct3         num_res
-				reportOut.println(pdbCode+pdbChainCode+"\t"+cutoff1+"\t"+cutoff2+"\t"+cutoff3+"\t"+ct1+"\t"+ct2+"\t"+ct3+"\t"+sequence.length()+"\t"+
+				String runId = "-";
+				if (benchmark) runId = pdbCode+pdbChainCode;
+				double cutoff1 = cutoffs[0], cutoff2 = 0, cutoff3 = 0;
+				String ct1 = cts[0], ct2 = "-", ct3 = "-";
+				if (cts.length>1) {
+					ct2 = cts[1];
+					cutoff2 = cutoffs[1];
+					if (cts.length>2) {
+						ct3 = cts[2];
+						cutoff3 = cutoffs[2];
+					}
+				}
+				//               run_id       cutoff      cutoff2      cutoff3      ct1      ct2      ct3         num_res
+				reportOut.println(runId+"\t"+cutoff1+"\t"+cutoff2+"\t"+cutoff3+"\t"+ct1+"\t"+ct2+"\t"+ct3+"\t"+sequence.length()+"\t"+
 				//  result_id     error_val         
 						i + "\t" + errStr + "\t" +
 				//   up_bound_viol    low_bound_viol   max_bound_up    max_bound_low      rms_bound
@@ -337,7 +406,7 @@ public class reconstruct {
 			}
 			reportOut.close();
 		} catch (FileNotFoundException e) {
-			System.err.println("Couldn't write to report file "+reportFile.getAbsolutePath() +". Error: "+e.getMessage());
+			System.err.println("Couldn't write to report file "+reportFile+". Error: "+e.getMessage());
 		}
 		
 	}
