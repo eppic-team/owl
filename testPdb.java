ced3175a1cb36e05f74965970fe0dad74434adae
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2008-11-04 10:21:49 +0000

    Cleaned up and updated the testPdb example code.

--- testPdb.java
+++ testPdb.java
@@ -1,56 +1,52 @@
+import java.io.IOException;
 import java.sql.SQLException;
-//import java.util.ArrayList;
+
+import edu.uci.ics.jung.graph.util.Pair;
 
 import proteinstructure.*;
 
-//import java.util.TreeMap;
-
+/**
+ * An example program for some of the classes in the proteinstructure package (Pdb, RIGraph, ...) 
+ * Loads structure data from pdbase and pdb file and calculates graph dumping contacts to file 
+ * 
+ */
 public class testPdb {
 
-	/**
-	 * Test class for classes in aglappe.proteinstructure package (Pdb, Graph, AA, Contact) 
-	 * Loads structure data from pdbase, msdsd and pdb file and calculates graph dumping contacts to file
-	 * Also gets contact map object and some common neighbours from it
-	 * @throws SQLException 
-	 */
-	
-	public static void main(String[] args) 
-		throws Exception{
+	public static void main(String[] args) throws SQLException, PdbCodeNotFoundError, PdbLoadError, IOException {
 		
-		String accode="1bxy";
-		String chaincode="A";
-		// data from pdbase
+		String pdbCode="1bxy";
+		String pdbChainCode="A";
+		
+		// data from pdbase database
 		System.out.println("loading structure from pdbase");
-		Pdb pdb = new PdbasePdb(accode); // by default it goes to database "pdbase", a name for another pdbase database + a MySQLConnection can also be passed
-		pdb.load(chaincode);
+		Pdb pdbFromPdbase = new PdbasePdb(pdbCode); // by default it goes to database "pdbase", a name for another pdbase database + a MySQLConnection can also be passed
+		pdbFromPdbase.load(pdbChainCode);
 		System.out.println("dumping structure to pdb file");
-		pdb.dump2pdbfile("testpdb.txt");
-		String chain = pdb.getChainCode();
+		pdbFromPdbase.dump2pdbfile("test_dump_from_pdbase.pdb");
+		// note that the chainCode is not necessarily the same as the pdbChainCode
+		String chainCode = pdbFromPdbase.getChainCode();
 		System.out.println("getting graph");
-		RIGraph graph = pdb.get_graph("ALL", 4.1);
-		System.out.println("dumping contacts to file");
-		graph.write_graph_to_file("test.txt");
-		System.out.println("getting start of contact map matrix from graph");
+		RIGraph graph = pdbFromPdbase.get_graph("ALL", 4.1);
+		System.out.println("writing graph to file");
+		graph.write_graph_to_file("test_from_pdbase.cm");
+		
+		// getting edges and some information from the end nodes
+		System.out.println("i\ti_res\tj\tj_res\tdistance");
+		for (RIGEdge edge:graph.getEdges()) {
+			Pair<RIGNode> pair = graph.getEndpoints(edge);
+			RIGNode nodei = pair.getFirst();
+			RIGNode nodej = pair.getSecond();
+			System.out.println(nodei.getResidueSerial()+"\t"+nodei.getResidueType()+"\t"+nodej.getResidueSerial()+"\t"+nodej.getResidueType()+"\t"+edge.getDistance());
+		}
 
-		// data from msdsd
-		System.out.println("loading structure from msdsd");
-		Pdb pdb2 = new MsdsdPdb(accode); // by default it goes to database "msdsd_00_07_a", a name for another msdsd database + a MySQLConnection can also be passed
-		pdb2.load(chaincode);
-		System.out.println("dumping structure to pdb file");
-		pdb2.dump2pdbfile("testpdb2.txt");
-		System.out.println("getting graph");
-		RIGraph graph2 = pdb2.get_graph("ALL", 4.1);
-		System.out.println("dumping contacts to file");
-		graph2.write_graph_to_file("test2.txt");
-
-		// data from pdb
+		// data from pdb file
 		System.out.println("reading from dumped pdb file");
-		Pdb pdb3 = new PdbfilePdb("testpdb.txt"); // we read from the file dumped from pdbase, note that dumped file contains internal chain identifier
-		pdb3.load(chain);
+		Pdb pdbFromFile = new PdbfilePdb("test_dump_from_pdbase.pdb"); // we read from the file dumped from pdbase, note that dumped file contains internal chain identifier
+		pdbFromFile.load(chainCode);
 		System.out.println("getting graph");
-		RIGraph graph3 = pdb3.get_graph("ALL", 4.1);
-		System.out.println("dumping contacts to file");
-		graph3.write_graph_to_file("test3.txt");
+		RIGraph graph2 = pdbFromFile.get_graph("ALL", 4.1);
+		System.out.println("writing graph to file");
+		graph2.write_graph_to_file("test_pdb_from_file.cm");
 	}
 
 }
