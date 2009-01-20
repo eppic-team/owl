eb8f2d43ac2f08e5be01ec4ac2c7d2fa18726322
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2009-01-20 12:01:34 +0000

    Changed the scoring so that is an error function: now it is the average sum square upper bounds deviation from the contact map. A perfect fit to the contact maps is err=0. Cleaned up, refactored and documented.

--- embed/Distiller.java
+++ embed/Distiller.java
@@ -1,6 +1,9 @@
 package embed;
 
-//import java.util.HashMap;
+import java.io.File;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.TreeSet;
@@ -14,22 +17,55 @@ import proteinstructure.RIGEdge;
 import proteinstructure.RIGraph;
 
 public class Distiller {
-
+	
 	private static final int DIAGONALS_TO_SKIP = 3;
 	
-	//private RIGraph rig;
+	
+	/**
+	 * Class to store a IntPairSet (set of contacts) together with its score.
+	 * Can be sorted based on the score order. 
+	 * @author duarte
+	 *
+	 */
+	private class SetScore implements Comparable<SetScore>{
+		public double score;
+		public IntPairSet set;
+		public SetScore(double score, IntPairSet set) {
+			this.score = score;
+			this.set = set;
+		}
+		public int compareTo(SetScore o) {
+			if (this.score<o.score) return -1;
+			if (this.score>o.score) return 1;
+			return 0;
+		}
+	}
+	
+	
+	/*-------------------------- members  -------------------------------*/
+	
 	private Bound[][] cmapBounds;
 	private int totalNumberContacts;
 	private double finalContacts;
 	private int cmapSize;
 	
+	private ArrayList<SetScore> allSampledSets;
+	
+	/*----------------------- constructors  -----------------------------*/
+	
+	/**
+	 * 
+	 * @param rig
+	 * @param finalContacts
+	 */
 	public Distiller(RIGraph rig, double finalContacts) {
-		//this.rig = rig;
 		this.cmapBounds = Reconstructer.convertRIGraphToBoundsMatrix(rig);
 		this.totalNumberContacts = rig.getEdgeCount();
 		this.finalContacts = finalContacts;
 		this.cmapSize = cmapBounds.length;
 	}
+	
+	/*------------------------- privates  -------------------------------*/
 	
 	/**
 	 * Calculates the sum square deviation of the given all pairs bounds matrix 
@@ -52,63 +88,31 @@ public class Distiller {
 	}
 	
 	/**
-	 * Scores the given sparseBounds matrix (a subset of the contact map) by first 
+	 * Measures the error function for the given sparseBounds matrix (a subset of the contact map) by first 
 	 * inferring bounds for all pairs through the triangle inequality and then 
 	 * measuring how well the all pairs matrix fits the contact map.  
-	 * Thus a high score means the given sparseBounds matrix has high information content
-	 * about the rest of the contact map and low score that the matrix has low 
+	 * Thus a high error value means the given sparseBounds matrix has low information content
+	 * about the rest of the contact map and low error value that the matrix has high 
 	 * information content  
 	 * @param sparseBounds
 	 * @return
 	 */
-	private double score(Bound[][] sparseBounds) {
+	private double getError(Bound[][] sparseBounds) {
 		// infer bounds for all pairs through triangle inequality
 		Bound[][] bounds = inferAllBounds(sparseBounds);
-		return 1/computeSumSquareDeviation(bounds); 
+		return (computeSumSquareDeviation(bounds))/(double) cmapSize; 
 	}
 	
+	/**
+	 * Infer bounds for all pairs from a spare bounds matrix via the triangle inequality
+	 * @param sparseBounds
+	 * @return
+	 */
 	private Bound[][] inferAllBounds(Bound[][] sparseBounds) {
 		BoundsSmoother bs = new BoundsSmoother(sparseBounds);
 		return bs.getInitialBoundsAllPairs();
 	}
 
-//	public IntPairSet distillGreedy() {
-//		HashMap<Pair<Integer>, Double> scores = new HashMap<Pair<Integer>, Double>();
-//		
-//		IntPairSet allCMPairs = getPairs(cmapBounds, DIAGONALS_TO_SKIP);
-//
-//		Pair<Integer> pairToRemove = findHighestScoringEdge(allCMPairs, BoundsSmoother.copyBounds(cmapBounds));
-//		//removePair(BoundsSmoother.copyBounds(cmapBounds));
-//				
-//		
-//		return null;
-//	}
-//	
-//	private Pair<Integer> findHighestScoringEdge(IntPairSet pairs, Bound[][] sparseBounds) {
-//		// we proceed removing one edge at a time and scoring the resulting all pairs matrix
-//		// (inferred via triangle inequality) to see how well it fits the contact map. 
-//		// A high score for a matrix (with a removed edge) means good fit to the contact map
-//		// Thus removing the edge that gives a matrix with the highest score means we remove
-//		// the edge which was least important in getting a good score.
-//		Pair<Integer> highestScEdge = null;
-//		double maxScore = 0;
-//		for (Pair<Integer> pair:pairs) {
-//			// we remove the pair
-//			removePair(sparseBounds,pair);
-//			// and score
-//			double score = score(sparseBounds);
-//			if (score>maxScore) {
-//				maxScore = score;
-//				highestScEdge = pair;
-//			}
-//		}
-//		return highestScEdge;
-//	}
-//	
-//	private void removePair(Bound[][] bounds, Pair<Integer> pair) {
-//		bounds[pair.getFirst()][pair.getSecond()] = null;
-//	}
-	
 	/**
 	 * Gets all pairs of the given bounds matrix except for the first 
 	 * minSeqSeparation diagonals, the indices of the pairs are the matrix 
@@ -129,31 +133,15 @@ public class Distiller {
 		return allCMPairs;
 	}
 	
-	public IntPairSet distillRandomSampling(int numSamples) {
-		double maxScore = 0;
-		int indexMaxScoringSample = -1;
-		Bound[][] bestScoringBounds = null;
-		for (int i=0;i<numSamples;i++) {
-			Bound[][] boundsSubset = sampleSubset();
-			double score = score(boundsSubset);
-			System.out.printf("sample "+i+": "+getPairs(boundsSubset,1).size()+" contacts. Score: %4.2e\n",score);
-			if (score>maxScore) {
-				bestScoringBounds = boundsSubset;
-				maxScore = score;
-				indexMaxScoringSample = i;
-			}
-		}
-		System.out.printf("Best score: %4.2e for sample %d\n", maxScore, indexMaxScoringSample);
-		return getPairs(bestScoringBounds, DIAGONALS_TO_SKIP);
-	}
-	
-	private Bound[][] sampleSubset() {
+	/**
+	 * Randomly samples one subset of numSampledContacts contacts from contacts with 
+	 * sequence separation above {@link #DIAGONALS_TO_SKIP} 
+	 * @param numSampledContacts
+	 * @return
+	 */
+	private Bound[][] sampleSubset(int numSampledContacts) {
 		Bound[][] subset = new Bound[cmapSize][cmapSize];
-
 		
-		int numSampledContacts = (int)(totalNumberContacts*finalContacts);
-		System.out.println("Sampling "+numSampledContacts+" contacts");
-
 		Random rand = new Random();
 		IntPairSet allcmpairs = getPairs(cmapBounds, DIAGONALS_TO_SKIP);
 		TreeSet<Integer> sampledIndices = new TreeSet<Integer>();
@@ -163,7 +151,7 @@ public class Distiller {
 			// we loop until the return value is true, i.e. we have a new index
 			while (!sampledIndices.add(rand.nextInt(allcmpairs.size())));
 		}
-		// the iterator returns the indices in order as it is from a TreeSet
+		// the iterator returns the indices in order since it is from a TreeSet
 		Iterator<Integer> indexIterator = sampledIndices.iterator();
 		int index = 0;
 		int sampledIndex = indexIterator.next();
@@ -183,27 +171,108 @@ public class Distiller {
 		return subset;
 	}
 	
+	/*-------------------------- publics  -------------------------------*/
+	
+	/**
+	 * Samples random subsets of the contact map scoring them and storing all results in the
+	 * allSampledSets ArrayList. Get the max and min scoring subsets with {@link #getMaxErrorSetScore()} and 
+	 * {@link #getMinErrorSetScore()}. Write all scores out with {@link #writeScores(PrintStream)}
+	 * @param numSamples
+	 */
+	public void distillRandomSampling(int numSamples) {
+		 
+		allSampledSets = new ArrayList<SetScore>();		
+
+		int numSampledContacts = (int)(totalNumberContacts*finalContacts);
+		System.out.println("Sampling "+numSamples+" subsets of "+numSampledContacts+" contacts ");
+		
+		for (int i=0;i<numSamples;i++) {
+			Bound[][] boundsSubset = sampleSubset(numSampledContacts);
+			double error = getError(boundsSubset);
+		
+			allSampledSets.add(new SetScore(error,getPairs(boundsSubset, DIAGONALS_TO_SKIP)));			
+			//System.out.printf("sample "+i+": "+getPairs(boundsSubset,1).size()+" contacts. Error value: %4.2e\n",error);
+			if (i!=0 && i%10==0) System.out.print(".");
+			if (i!=0 && i%1000==0) System.out.println(" "+i);
+		}
+				
+	}
+	
+	/**
+	 * Returns the SetScore with the minimum score of all samples
+	 * @return
+	 */
+	public SetScore getMinErrorSetScore() {
+		return Collections.min(allSampledSets);
+	}
+
+	/**
+	 * Returns the SetScore with the maximum score of all samples
+	 * @return
+	 */
+	public SetScore getMaxErrorSetScore() {
+		return Collections.max(allSampledSets);
+	}
+	
+	/**
+	 * Writes to the given PrintStream all the scores in a 1 column text file
+	 * @param out
+	 */
+	public void writeScores(PrintStream out) {
+		for (SetScore ss:allSampledSets) {
+			out.printf("%8.3f\n",ss.score);
+		}
+	}
+		
+	/*------------------------- statics  -----------------------------*/
+	
+	private static RIGraph createRIGraphFromIntPairSet(String sequence, IntPairSet set) {
+		RIGraph graph = new RIGraph(sequence);
+		for (Pair<Integer> pair:set) {
+			graph.addEdge(new RIGEdge(1.0), graph.getNodeFromSerial(pair.getFirst()+1), graph.getNodeFromSerial(pair.getSecond()+1));
+		}
+		return graph;
+	}
+	
 	/*-------------------------- main  -------------------------------*/
 	
 	public static void main(String[] args) throws Exception {
+		
+		if (args.length<1) {
+			System.err.println("Usage: Distiller <num_samples>");
+		}
+		int numSamples = Integer.parseInt(args[0]);
+		
+		
 		String pdbCode = "1sha";
 		String pdbChainCode = "A";
 		String ct = "Ca";
 		double cutoff = 8.0;
 
+		File essenceFile = new File("/project/StruPPi/jose/embed/"+pdbCode+pdbChainCode+"_essence.cm");
+		File maxErrorFile = new File("/project/StruPPi/jose/embed/"+pdbCode+pdbChainCode+"_maxError.cm");
+		File scoresFile = new File("/project/StruPPi/jose/embed/"+pdbCode+pdbChainCode+".scores");
+		
 		Pdb pdb = new PdbasePdb(pdbCode);
 		pdb.load(pdbChainCode);
 		RIGraph graph = pdb.get_graph(ct, cutoff);
 		System.out.println("Total contacts: "+graph.getEdgeCount());
 		
 		Distiller dist = new Distiller(graph, 0.05);
-		//dist.distillGreedy();
-		IntPairSet bestSet = dist.distillRandomSampling(1000);
-		RIGraph essence = new RIGraph(pdb.getSequence());
-		for (Pair<Integer> pair:bestSet) {
-			essence.addEdge(new RIGEdge(1.0), essence.getNodeFromSerial(pair.getFirst()+1), essence.getNodeFromSerial(pair.getSecond()+1));
-			//System.out.println(pair.getFirst()+"\t"+pair.getSecond());
-		}
-		essence.write_graph_to_file("/project/StruPPi/jose/embed/"+pdbCode+pdbChainCode+"_essence.cm");
+		
+		dist.distillRandomSampling(numSamples);
+
+		SetScore maxErrorSetScore = dist.getMaxErrorSetScore();
+		SetScore minErrorSetScore = dist.getMinErrorSetScore();
+
+		System.out.println();
+		
+		System.out.printf("Max error: %4.2e \n", maxErrorSetScore.score);
+		createRIGraphFromIntPairSet(pdb.getSequence(), maxErrorSetScore.set).write_graph_to_file(maxErrorFile.getAbsolutePath());
+
+		System.out.printf("Min error: %4.2e \n", minErrorSetScore.score);
+		createRIGraphFromIntPairSet(pdb.getSequence(), minErrorSetScore.set).write_graph_to_file(essenceFile.getAbsolutePath());
+				
+		dist.writeScores(new PrintStream(scoresFile));
 	}
 }
