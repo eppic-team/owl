ad778884f7b445e9c6a899097a45217a8c15f33d
Henning Stehr <h.stehr@gmail.com>
2009-08-07 13:26:20 +0000

    adding compareTo method to compare two IntervalSets

--- proteinstructure/IntervalSet.java
+++ proteinstructure/IntervalSet.java
@@ -1,15 +1,17 @@
 package proteinstructure;
 
+import java.util.Iterator;
 import java.util.TreeSet;
 
 /**
  * Class representing a unique, ordered (according to 
  * ordering defined in Interval.compareTo) set of intervals
- *
  */
-public class IntervalSet extends TreeSet<Interval>{
+public class IntervalSet extends TreeSet<Interval> implements Comparable<IntervalSet> {
 
 	private static final long serialVersionUID = 1L;
+		
+	/*---------------------------- public methods ---------------------------*/
 	
 	/**
 	 * Returns an ordered set of integers resulting from the intersection of all Intervals in this set
@@ -23,6 +25,33 @@ public class IntervalSet extends TreeSet<Interval>{
 			}
 		}
 		return set; 
+	}
+	
+	/*-------------------------- implemented methods ------------------------*/
+	
+	/**
+	 * Compares two IntevalSets according to a lexicographical-like ordering.
+	 */
+	public int compareTo(IntervalSet other) {
+		// whichever set has a smaller first element is smaller
+		int compare = this.first().compareTo(other.first());
+		if(compare != 0) return compare;
+		// otherwise, we have to go through all elements until one is missing
+		TreeSet<Integer> thisElements = this.getIntegerSet();
+		TreeSet<Integer> otherElements = other.getIntegerSet();
+		Iterator<Integer> thisIt = thisElements.iterator();
+		Iterator<Integer> otherIt = otherElements.iterator();
+		while(thisIt.hasNext() && otherIt.hasNext()) {
+			int thisNext = thisIt.next();
+			int otherNext = otherIt.next();
+			if(thisNext != otherNext) {
+				return new Integer(thisNext).compareTo(new Integer(otherNext));
+			}
+		}
+		if(thisIt.hasNext() && !otherIt.hasNext()) return 1;	// this is longer, other ends
+		if(!thisIt.hasNext() && otherIt.hasNext()) return -1;	// this ends, other is longer
+		// if none of the above checks could break the tie, then we are equal
+		return 0;
 	}
 	
 }
