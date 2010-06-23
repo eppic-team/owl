723aacda39a5c2ef1f9f482f277cf4e0142b83af
Corinna Vehlow <c.vehlow@gmx.de>
2010-06-23 10:24:30 +0000

    Additional method for coordinate transversion.

--- src/owl/core/structure/graphs/RIGGeometry.java
+++ src/owl/core/structure/graphs/RIGGeometry.java
@@ -8,7 +8,7 @@ import javax.vecmath.Vector3d;
 
 import edu.uci.ics.jung.graph.util.Pair;
 
-import owl.core.structure.Atom;
+//import owl.core.structure.Atom;
 import owl.core.structure.Residue;
 import owl.gmbp.GmbpGeometry;
 
@@ -18,7 +18,7 @@ public class RIGGeometry {
 	
 	private RIGraph graph;
 	private TreeMap<Integer, Residue> residues;
-	private HashMap<Integer[],Vector3d> ca_coord_sph_rotated;  // holds geometry (translated and rotated contact coordinates of CA-position
+	private HashMap<String,Vector3d> ca_coord_sph_rotated;  // holds geometry (translated and rotated contact coordinates of CA-position
 							//of j-Residue with respect to central iResidue of each edge of the graph, defined by residue serials of edge) 
 	
 	
@@ -40,7 +40,7 @@ public class RIGGeometry {
 		GmbpGeometry gmbp = new GmbpGeometry();
 		
 //		HashMap<String,Vector3d> ca_coord_sph_rotated = new HashMap<String, Vector3d>();
-		ca_coord_sph_rotated = new HashMap<Integer[],Vector3d>();
+		ca_coord_sph_rotated = new HashMap<String,Vector3d>();
 		for (RIGEdge edge:graph.getEdges()) {
 			edgeNum++;
 			// extract (x,y,z) coordinates for nodes of both end of edge
@@ -54,28 +54,33 @@ public class RIGGeometry {
 			
 			Residue iRes = this.residues.get(iNum);
 			Residue jRes = this.residues.get(jNum);
-			Atom iAtom = iRes.getAtom("CA");
-			Atom jAtom = jRes.getAtom("CA");
-			Point3d iCoordCA = iAtom.getCoords();
-			Point3d jCoordCA = jAtom.getCoords();
+			
+			// Testoutput
+//			Atom iAtom = iRes.getAtom("CA");
+//			Atom jAtom = jRes.getAtom("CA");
+//			Point3d iCoordCA = iAtom.getCoords();
+//			Point3d jCoordCA = jAtom.getCoords();
 //			System.out.println("EdgeNr="+edgeNum+" between "+iNum+iResType+"("+iCoordCA.x+","+iCoordCA.y+","+iCoordCA.z+")"
 //					+" and "+jNum+jResType+"("+jCoordCA.x+","+jCoordCA.y+","+jCoordCA.z+")");
-			System.out.printf("EdgeNr= %s between %s%s %s and %s%s %s  ",edgeNum,iNum,iResType,iCoordCA,jNum,jResType,jCoordCA);
+//			System.out.printf("EdgeNr= %s between %s%s %s and %s%s %s  ",edgeNum,iNum,iResType,iCoordCA,jNum,jResType,jCoordCA);
 			
 			
 			// translate coordinates with rotation and translation invariant framework
 			HashMap<String,Point3d> iCoord = gmbp.getTheResidueCoord(iRes);
 			HashMap<String,Point3d> jCoord = gmbp.getTheResidueCoord(jRes);
 			
-			Vector3d CA_coord = new Vector3d(0,0,0);
-			
+			Vector3d CA_coord_I = new Vector3d(0,0,0);
+			Vector3d CA_coord_J = new Vector3d(0,0,0);
 			// LEAVE this EDGE and CONTINUE with next one if the above condition is not satisfied.
-			HashMap<String,Vector3d> atom_coord_rotated = new HashMap<String, Vector3d>();
+			HashMap<String,Vector3d> atom_coord_rotated_I = new HashMap<String, Vector3d>();
+			HashMap<String,Vector3d> atom_coord_rotated_J = new HashMap<String, Vector3d>();
 			if (iCoord!=null && jCoord!=null) {
 				// METHOD FOR EXTRACTING THE NEIGHBOR'S TRANSLATED-ROTATED COORDINATES //
-				atom_coord_rotated = gmbp.getNeighborsTransRotatedCoord(iCoord, jCoord, iResType, jResType, iRes, jRes, true);
+				atom_coord_rotated_I = gmbp.getNeighborsTransRotatedCoord(jCoord, iCoord, jResType, iResType, jRes, iRes, true);
+				atom_coord_rotated_J = gmbp.getNeighborsTransRotatedCoord(iCoord, jCoord, iResType, jResType, iRes, jRes, true);
 				//========= C-ALPHA and C-coordinates ================// 
-				CA_coord = atom_coord_rotated.get("CA");
+				CA_coord_I = atom_coord_rotated_I.get("CA");
+				CA_coord_J = atom_coord_rotated_J.get("CA");
 			}
 			else {
 				continue;
@@ -83,18 +88,30 @@ public class RIGGeometry {
 			
 			// GET the SPHERICAL COORDINATES for CA, C, CB, and CG using METHOD "getSphericalFromCartesian", 
 			// if these coordinates exist (are non-zero).
-			Vector3d CA_coord_sph = new Vector3d(0,0,0);
-			if (!CA_coord.equals(new Vector3d(0.0,0.0,0.0))) {
-				CA_coord_sph = gmbp.getSphericalFromKartesian(CA_coord); // (r,theta,phi) // (r, phi,lambda)
+			Vector3d CA_coord_sph_I = new Vector3d(0,0,0);
+			Vector3d CA_coord_sph_J = new Vector3d(0,0,0);
+			if (!CA_coord_J.equals(new Vector3d(0.0,0.0,0.0))) {
+				CA_coord_sph_I = gmbp.getSphericalFromCartesian(CA_coord_I); // (r,theta,phi) // (r, phi,lambda)
+				CA_coord_sph_J = gmbp.getSphericalFromCartesian(CA_coord_J); // (r,theta,phi) // (r, phi,lambda)
 			}
 			
 			// Save translated and rotated coordinate of contact
-			ca_coord_sph_rotated.put(new Integer[]{iNum,jNum}, CA_coord_sph);
+			String key = String.valueOf(jNum)+"_"+String.valueOf(iNum);
+			ca_coord_sph_rotated.put(key, CA_coord_sph_I);
+			key = String.valueOf(iNum)+"_"+String.valueOf(jNum);
+			ca_coord_sph_rotated.put(key, CA_coord_sph_J);
+			if (!ca_coord_sph_rotated.containsKey(key)){
+				System.out.println("Something is going wrong");
+			}
 			
 //			System.out.println("TransRotCoord Cartesian: "+CA_coord.x+","+CA_coord.y+","+CA_coord.z
 //					+" SPH: "+CA_coord_sph.x+","+CA_coord_sph.y+","+CA_coord_sph.z);
-			System.out.printf("TransRotCoord Cartesian: %s Spherical: %s \n", CA_coord, CA_coord_sph);
+//			System.out.printf("TransRotCoord i->j Cartesian: %s Spherical: %s   j->i Cartesian: %s Spherical: %s \n", CA_coord_I, CA_coord_sph_I, CA_coord_J, CA_coord_sph_J);
 		}
+		
+//		if (ca_coord_sph_rotated.containsKey(new Integer[]{iNum,jNum})){
+//			Vector3d CA_coord_sph = ca_coord_sph_rotated.get(new Integer[]{iNum,jNum});
+//		}
 		
 //		// iterate over translated coordinates
 //		for (Entry<Integer[], Vector3d> entry : ca_coord_sph_rotated.entrySet()) {
@@ -113,7 +130,7 @@ public class RIGGeometry {
 	}
 	
 	// --------- getters ----------
-	public HashMap<Integer[],Vector3d> getRotatedCoordOfContacts(){
+	public HashMap<String,Vector3d> getRotatedCoordOfContacts(){
 		return this.ca_coord_sph_rotated;
 	}
 
