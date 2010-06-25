7e5d53160e9f57e6976b137d5ddf4b5b7dd48fbf
Corinna Vehlow <c.vehlow@gmx.de>
2010-06-25 13:40:27 +0000

    Check for consistency in translation rotation framework.

--- src/owl/core/structure/graphs/RIGGeometry.java
+++ src/owl/core/structure/graphs/RIGGeometry.java
@@ -8,7 +8,6 @@ import javax.vecmath.Vector3d;
 
 import edu.uci.ics.jung.graph.util.Pair;
 
-//import owl.core.structure.Atom;
 import owl.core.structure.Residue;
 import owl.gmbp.GmbpGeometry;
 
@@ -18,8 +17,9 @@ public class RIGGeometry {
 	
 	private RIGraph graph;
 	private TreeMap<Integer, Residue> residues;
-	private HashMap<String,Vector3d> ca_coord_sph_rotated;  // holds geometry (translated and rotated contact coordinates of CA-position
-							//of j-Residue with respect to central iResidue of each edge of the graph, defined by residue serials of edge) 
+	private HashMap<String,Vector3d> coord_sph_rotated;  // holds geometry (translated and rotated contact coordinates of CA-position
+							//of j-Residue with respect to central iResidue of each edge of the graph, defined by residue serials of edge)
+	private String atomType = "CA";
 	
 	
 	/**
@@ -29,6 +29,19 @@ public class RIGGeometry {
 	public RIGGeometry(RIGraph graph, TreeMap<Integer, Residue> residues) {
 		this.graph = graph;
 		this.residues = residues;
+		this.atomType = "CA";
+		
+		initialiseGeometry();
+	}
+	
+	/**
+	 * Constructs a RIGraph with a sequence but no edges
+	 * @param sequence
+	 */
+	public RIGGeometry(RIGraph graph, TreeMap<Integer, Residue> residues, String atomType) {
+		this.graph = graph;
+		this.residues = residues;
+		this.atomType = atomType;
 		
 		initialiseGeometry();
 	}
@@ -39,8 +52,8 @@ public class RIGGeometry {
 		int edgeNum = 0;
 		GmbpGeometry gmbp = new GmbpGeometry();
 		
-//		HashMap<String,Vector3d> ca_coord_sph_rotated = new HashMap<String, Vector3d>();
-		ca_coord_sph_rotated = new HashMap<String,Vector3d>();
+//		HashMap<String,Vector3d> coord_sph_rotated = new HashMap<String, Vector3d>();
+		coord_sph_rotated = new HashMap<String,Vector3d>();
 		for (RIGEdge edge:graph.getEdges()) {
 			edgeNum++;
 			// extract (x,y,z) coordinates for nodes of both end of edge
@@ -69,8 +82,8 @@ public class RIGGeometry {
 			HashMap<String,Point3d> iCoord = gmbp.getTheResidueCoord(iRes);
 			HashMap<String,Point3d> jCoord = gmbp.getTheResidueCoord(jRes);
 			
-			Vector3d CA_coord_I = new Vector3d(0,0,0);
-			Vector3d CA_coord_J = new Vector3d(0,0,0);
+			Vector3d coord_I = new Vector3d(0,0,0);
+			Vector3d coord_J = new Vector3d(0,0,0);
 			// LEAVE this EDGE and CONTINUE with next one if the above condition is not satisfied.
 			HashMap<String,Vector3d> atom_coord_rotated_I = new HashMap<String, Vector3d>();
 			HashMap<String,Vector3d> atom_coord_rotated_J = new HashMap<String, Vector3d>();
@@ -79,8 +92,8 @@ public class RIGGeometry {
 				atom_coord_rotated_I = gmbp.getNeighborsTransRotatedCoord(jCoord, iCoord, jResType, iResType, jRes, iRes, true);
 				atom_coord_rotated_J = gmbp.getNeighborsTransRotatedCoord(iCoord, jCoord, iResType, jResType, iRes, jRes, true);
 				//========= C-ALPHA and C-coordinates ================// 
-				CA_coord_I = atom_coord_rotated_I.get("CA");
-				CA_coord_J = atom_coord_rotated_J.get("CA");
+				coord_I = atom_coord_rotated_I.get(this.atomType); //("CA");
+				coord_J = atom_coord_rotated_J.get(this.atomType); //("CA");
 			}
 			else {
 				continue;
@@ -88,33 +101,33 @@ public class RIGGeometry {
 			
 			// GET the SPHERICAL COORDINATES for CA, C, CB, and CG using METHOD "getSphericalFromCartesian", 
 			// if these coordinates exist (are non-zero).
-			Vector3d CA_coord_sph_I = new Vector3d(0,0,0);
-			Vector3d CA_coord_sph_J = new Vector3d(0,0,0);
-			if (!CA_coord_J.equals(new Vector3d(0.0,0.0,0.0))) {
-				CA_coord_sph_I = gmbp.getSphericalFromCartesian(CA_coord_I); // (r,theta,phi) // (r, phi,lambda)
-				CA_coord_sph_J = gmbp.getSphericalFromCartesian(CA_coord_J); // (r,theta,phi) // (r, phi,lambda)
+			Vector3d coord_sph_I = new Vector3d(0,0,0);
+			Vector3d coord_sph_J = new Vector3d(0,0,0);
+			if (!coord_J.equals(new Vector3d(0.0,0.0,0.0))) {
+				coord_sph_I = gmbp.getSphericalFromCartesian(coord_I); // (r,theta,phi) // (r, phi,lambda)
+				coord_sph_J = gmbp.getSphericalFromCartesian(coord_J); // (r,theta,phi) // (r, phi,lambda)
 			}
 			
 			// Save translated and rotated coordinate of contact
 			String key = String.valueOf(jNum)+"_"+String.valueOf(iNum);
-			ca_coord_sph_rotated.put(key, CA_coord_sph_I);
+			coord_sph_rotated.put(key, coord_sph_I);
 			key = String.valueOf(iNum)+"_"+String.valueOf(jNum);
-			ca_coord_sph_rotated.put(key, CA_coord_sph_J);
-			if (!ca_coord_sph_rotated.containsKey(key)){
+			coord_sph_rotated.put(key, coord_sph_J);
+			if (!coord_sph_rotated.containsKey(key)){
 				System.out.println("Something is going wrong");
 			}
 			
-//			System.out.println("TransRotCoord Cartesian: "+CA_coord.x+","+CA_coord.y+","+CA_coord.z
-//					+" SPH: "+CA_coord_sph.x+","+CA_coord_sph.y+","+CA_coord_sph.z);
+//			System.out.println("TransRotCoord Cartesian: "+coord.x+","+coord.y+","+coord.z
+//					+" SPH: "+coord_sph.x+","+coord_sph.y+","+coord_sph.z);
 //			System.out.printf("TransRotCoord i->j Cartesian: %s Spherical: %s   j->i Cartesian: %s Spherical: %s \n", CA_coord_I, CA_coord_sph_I, CA_coord_J, CA_coord_sph_J);
 		}
 		
-//		if (ca_coord_sph_rotated.containsKey(new Integer[]{iNum,jNum})){
-//			Vector3d CA_coord_sph = ca_coord_sph_rotated.get(new Integer[]{iNum,jNum});
+//		if (coord_sph_rotated.containsKey(new Integer[]{iNum,jNum})){
+//			Vector3d CA_coord_sph = coord_sph_rotated.get(new Integer[]{iNum,jNum});
 //		}
 		
 //		// iterate over translated coordinates
-//		for (Entry<Integer[], Vector3d> entry : ca_coord_sph_rotated.entrySet()) {
+//		for (Entry<Integer[], Vector3d> entry : coord_sph_rotated.entrySet()) {
 //  		    System.out.printf("Contact %s,%s coord: %s \n", entry.getKey()[0], entry.getKey()[1], entry
 // 					.getValue());
 //  		}
@@ -131,7 +144,11 @@ public class RIGGeometry {
 	
 	// --------- getters ----------
 	public HashMap<String,Vector3d> getRotatedCoordOfContacts(){
-		return this.ca_coord_sph_rotated;
+		return this.coord_sph_rotated;
+	}
+	
+	public String getAtomType(){
+		return this.atomType;
 	}
 
 }
