bcc4cf1425552377aa3ee0d921f0887b60913092
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2011-01-26 10:32:20 +0000

    Methods to serialize/deserialize from file ChainInterface

--- src/owl/core/structure/ChainInterface.java
+++ src/owl/core/structure/ChainInterface.java
@@ -1,8 +1,12 @@
 package owl.core.structure;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.io.Serializable;
 
@@ -503,4 +507,21 @@ public class ChainInterface implements Comparable<ChainInterface>, Serializable 
 	public boolean isRimAndCoreZoomed() {
 		return zoomingUsed;
 	}
+	
+    public void serialize(File serializedFile) throws IOException {
+        FileOutputStream fileOut = new FileOutputStream(serializedFile);
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(this);
+        out.close();
+        fileOut.close();
+    }
+
+    public static ChainInterface readFromFile(File serialized) throws IOException, ClassNotFoundException {
+    	FileInputStream fileIn = new FileInputStream(serialized);
+    	ObjectInputStream in = new ObjectInputStream(fileIn);
+    	ChainInterface interfSc = (ChainInterface) in.readObject();
+    	in.close();
+    	fileIn.close();
+    	return interfSc;
+    }
 }
