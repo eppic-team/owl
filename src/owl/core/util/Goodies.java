1d1e0683e8a23e85c0895638c85933d7b0914db2
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2011-02-17 08:58:55 +0000

    Methods to serialize/deserialize from file

--- src/owl/core/util/Goodies.java
+++ src/owl/core/util/Goodies.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -124,4 +126,33 @@ public class Goodies {
 		}
 	}
 
+	/**
+	 * To serialize to file a given Serializable object
+	 * @param serializedFile
+	 * @param obj
+	 * @throws IOException
+	 */
+	public static void serialize(File serializedFile, Object obj) throws IOException {
+		FileOutputStream fileOut = new FileOutputStream(serializedFile);
+		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		out.writeObject(obj);
+		out.close();
+		fileOut.close();
+	}
+
+	/**
+	 * To deserialize from file a Serializable object. The returned object must be cast to the appropriate class.
+	 * @param serialized
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public static Object readFromFile(File serialized) throws IOException, ClassNotFoundException {
+		FileInputStream fileIn = new FileInputStream(serialized);
+		ObjectInputStream in = new ObjectInputStream(fileIn);
+		Object obj = in.readObject();
+		in.close();
+		fileIn.close();
+		return obj;
+	}
 }
