4bcf4aae9ca8bc4dc8df7dfc33df0b88f2eddb9b
Henning Stehr <h.stehr@tuhh.de>
2011-04-10 11:10:30 +0000

    adding package-info for owl.mutanom

---
+++ src/owl/mutanom/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * This package contains the source code for the methods described in:
+ * 
+ * The structural impact of cancer-associated mutations in oncogenes and tumor suppressors (2011).
+ * Henning Stehr*, Seon-Hi J. Jang*, Jose M. Duarte, Christoph Wierling, Hans Lehrach, Michael Lappe, Bodo M.H. Lange.
+ * submitted to Molecular Cancer, *shared first authorship
+ *
+ * The entry classes are GenePreSelectionTools, MainAnalysisPrepare and MainAnalysis. They depend on some
+ * external datasets currently found in /project/StruPPi/henning/project/mutanom/analysis/.
+ * 
+ * Overview of sub-packages:
+ * owl.mutanom.core		some core classes with general functionality
+ * owl.mutanom.output	classes for output visualization
+ * 
+ * The classe from these two subpackages can be useful in other contexts and could
+ * be incorporated in the owl.core hierarchy. This would require some clean-up and
+ * refactoring which hasn't been done yet.
+ * 
+ * @author Henning Stehr, stehr@molgen.mpg.de, 2011-04-10
+ */
+package owl.mutanom;
