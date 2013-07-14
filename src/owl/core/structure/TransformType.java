1030b81673dd48e896ff07143b14e87e394958c5
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2013-07-14 20:48:30 +0000

    Added isInfinite field

--- src/owl/core/structure/TransformType.java
+++ src/owl/core/structure/TransformType.java
@@ -2,44 +2,46 @@ package owl.core.structure;
 
 public enum TransformType {
 
-	AU				(0,   1, false, "AU"), 
-	XTALTRANSL		(1,   1, false, "XT"),  // translation
-	CELLTRANSL		(2,   1, false, "FT"),  // fractional translation 
+	AU				(0,   1, false, false, "AU"), 
+	XTALTRANSL		(1,   1, false, true, "XT"),  // translation
+	CELLTRANSL		(2,   1, false, true, "FT"),  // fractional translation 
 	
-	TWOFOLD			(3,   2, false, "2" ), 
-	TWOFOLDSCREW	(4,   2, true , "2S"), 
+	TWOFOLD			(3,   2, false, false, "2" ), 
+	TWOFOLDSCREW	(4,   2, true , true,  "2S"), 
 	
-	THREEFOLD		(5,   3, false, "3" ), 
-	THREEFOLDSCREW	(6,   3, true,  "3S"),
+	THREEFOLD		(5,   3, false, false, "3" ), 
+	THREEFOLDSCREW	(6,   3, true,  true,  "3S"),
 	
-	FOURFOLD		(7,   4, false, "4" ), 
-	FOURFOLDSCREW	(8,   4, true,  "4S"), 
+	FOURFOLD		(7,   4, false, false, "4" ), 
+	FOURFOLDSCREW	(8,   4, true,  true,  "4S"), 
 	
-	SIXFOLD       	(9,   6, false, "6" ), 
-	SIXFOLDSCREW  	(10,  6, true,  "6S"),
+	SIXFOLD       	(9,   6, false, false, "6" ), 
+	SIXFOLDSCREW  	(10,  6, true,  true,  "6S"),
 	
-	ONEBAR          (11, -1, false, "-1"),
+	ONEBAR          (11, -1, false, false, "-1"),
 	
-	TWOBAR          (12, -2, false, "-2"),
-	GLIDE           (13, -2, true,  "GL"),
+	TWOBAR          (12, -2, false, false, "-2"),
+	GLIDE           (13, -2, true,  false, "GL"),
 	
-	THREEBAR        (14, -3, false, "-3"),
+	THREEBAR        (14, -3, false, false, "-3"),
 	
-	FOURBAR         (15, -4, false, "-4"),
+	FOURBAR         (15, -4, false, false, "-4"),
 	
-	SIXBAR          (16, -6, false, "-6");
+	SIXBAR          (16, -6, false, false, "-6");
 	
 	
 	
 	private int id;
 	private int foldType;
 	private boolean isScrew;
+	private boolean isInfinite;
 	private String shortName;
 	
-	private TransformType(int id, int foldType, boolean isScrew, String shortName) {
+	private TransformType(int id, int foldType, boolean isScrew, boolean isInfinite, String shortName) {
 		this.id = id;
 		this.foldType = foldType;
 		this.isScrew = isScrew;
+		this.isInfinite = isInfinite;
 		this.shortName = shortName;		
 	}
 	
@@ -52,11 +54,20 @@ public enum TransformType {
 	}
 	
 	/**
-	 * Tells whether the transform is a screw or glide plane
+	 * Tells whether the transform type is a screw or glide plane
 	 * @return
 	 */
 	public boolean isScrew() {
 		return isScrew;
+	}
+	
+	/**
+	 * Tells whether the transform type produces infinite assemblies 
+	 * if interface happens between identical chains
+	 * @return
+	 */
+	public boolean isInfinite() {
+		return isInfinite;
 	}
 	 
 	public String getShortName() {
